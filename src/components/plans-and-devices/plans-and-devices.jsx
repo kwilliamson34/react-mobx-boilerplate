68ce74bb19383026419fb1a07c612c1f1cff3c90
Evan Kanter <ekanter@sapient.com>
2017-04-21 16:54:19 -0400

    Plans and Devices refactor to bootstrap breakpoints

--- src/components/plans-and-devices/plans-and-devices.jsx
+++ src/components/plans-and-devices/plans-and-devices.jsx
@@ -6,34 +6,36 @@ export default class PlansAndDevices extends React.Component {
     render() {
         return (
             <section className="placeholder mp-links">
-                <div className="container max-container">
+                <div className="container pad-header-spacing">
                     <h2>Shop Plans and Devices</h2>
                     <div className="row">
-
-                        <div className="col-xs-12 col-sm-12 col-md-4 text-left device-row">
-                            <div className="imagebox view-solutions">
-                                <h3>Solutions</h3>
-                                <p>Lorem ipsum dolor sit amet consectetuer adipiscing elit</p>
-                                <Link to="view-solutions" className="pad-button btn btn-lg btn-default">View Solutions</Link>
+                        <div className="col-xs-12 col-md-4 pad-box view-solutions">
+                            <div className="pad-padding text-left">
+                                <div className="pad-text imagebox">
+                                    <h3>Solutions</h3>
+                                    <p>Lorem ipsum dolor sit amet consectetuer adipiscing elit</p>
+                                    <Link to="view-solutions" className="pad-button btn btn-lg btn-default">View Solutions</Link>
+                                </div>
                             </div>
                         </div>
-
-                        <div className="col-xs-12 col-sm-12 col-md-4 text-left device-row">
-                            <div className="imagebox rate-plans">
-                                <h3>Rate Plans</h3>
-                                <p>Lorem ipsum dolor sit amet consectetuer adipiscing elit</p>
-                                <Link to="view-solutions" className="pad-button btn btn-lg btn-default">Shop Plans</Link>
+                        <div className="col-xs-12 col-md-4 pad-box rate-plans">
+                            <div className="pad-padding text-left">
+                                <div className="pad-text imagebox">
+                                    <h3>Rate Plans</h3>
+                                    <p>Lorem ipsum dolor sit amet consectetuer adipiscing elit</p>
+                                    <Link to="view-solutions" className="pad-button btn btn-lg btn-default">Shop Plans</Link>                                
+                                </div>
                             </div>
                         </div>
-
-                        <div className="col-xs-12 col-sm-12 col-md-4 text-left device-row">
-                            <div className="imagebox shop-devices">
-                                <h3>Devices & Accessories</h3>
-                                <p>Lorem ipsum dolor sit amet consectetuer adipiscing elit</p>
-                                <Link to="view-solutions" className="pad-button btn btn-lg btn-default">Shop Devices</Link>
+                        <div className="col-xs-12 col-md-4 pad-box shop-devices">
+                            <div className="pad-padding text-left">
+                                <div className="pad-text imagebox">
+                                    <h3>Devices & Accessories</h3>
+                                    <p>Lorem ipsum dolor sit amet consectetuer adipiscing elit</p>
+                                    <Link to="view-solutions" className="pad-button btn btn-lg btn-default">Shop Devices</Link>                                
+                                </div>
                             </div>
                         </div>
-
                     </div>
                 </div>
             </section>
