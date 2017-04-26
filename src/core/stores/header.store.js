fa934e6b14f4898fd2afc28aeaba412459973ec1
Paul Arce <parce@sapient.com>
2017-04-26 13:13:50 -0400

    * updates based on PR comments(tabbing, removed !important) * moved to using sass vars for white/black * removed unused dom manipulation in store

--- src/core/stores/header.store.js
+++ src/core/stores/header.store.js
@@ -1,17 +1,10 @@
 import { action, observable } from 'mobx';
 //import { apiService } from '../services/api.service';
 
-const $ = window.$;
-
 class HeaderStore {
 
   @action toggleMainMenu (){
     this.mainMenuIsOpen = !this.mainMenuIsOpen;
-    if(this.mainMenuIsOpen){
-      $('.pageMask').show();
-    }else{
-      $('.pageMask').hide();
-    }
   }
 
   @observable mainMenuIsOpen = false;
