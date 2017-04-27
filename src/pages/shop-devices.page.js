dac84ee0614fd58d5e7e5bf5615a92c0fd765cb9
mlgantt <mgantt@sapient.com>
2017-04-27 10:16:03 -0400

    device pages

--- src/pages/shop-devices.page.js
+++ src/pages/shop-devices.page.js
@@ -17,20 +17,20 @@ export default class ShopPlansView extends React.Component {
                 <section className="product-row">
                     <div className="container">
                         <h1>Phones</h1>
-                        <div className="card-container has-shadow">
-                            <span></span>
+                        <div className="product-container has-shadow">
+                            <span className="product-label">Apple iPhone 7</span>
                             <img src="" alt=""></img>
                         </div>
-                        <div className="card-container has-shadow">
-                            <span></span>
+                        <div className="product-container has-shadow">
+                            <span className="product-label">Samsung Galaxy S7</span>
                             <img src="" alt=""></img>
                         </div>
-                        <div className="card-container has-shadow">
-                            <span></span>
+                        <div className="product-container has-shadow">
+                            <span className="product-label">LG V20</span>
                             <img src="" alt=""></img>
                         </div>
-                        <div className="card-container has-shadow">
-                            <span></span>
+                        <div className="product-container has-shadow">
+                            <span className="product-label">Kyocera Duraforce PRO</span>
                             <img src="" alt=""></img>
                         </div>
                         <Button href="#link" className='fn-primary'>Explore all smartphones</Button>
@@ -39,20 +39,20 @@ export default class ShopPlansView extends React.Component {
                 <section className="product-row">
                     <div className="container">
                         <h1>Tablets</h1>
-                        <div className="card-container has-shadow">
-                            <span></span>
+                        <div className="product-container has-shadow">
+                            <span className="product-label">Apple iPad Pro (9.7 Inch)</span>
                             <img src="" alt=""></img>
                         </div>
-                        <div className="card-container has-shadow">
-                            <span></span>
+                        <div className="product-container has-shadow">
+                            <span className="product-label">Samsung Galaxy Tab E</span>
                             <img src="" alt=""></img>
                         </div>
-                        <div className="card-container has-shadow">
-                            <span></span>
+                        <div className="product-container has-shadow">
+                            <span className="product-label">LG G Pad X 8.0</span>
                             <img src="" alt=""></img>
                         </div>
-                        <div className="card-container has-shadow">
-                            <span></span>
+                        <div className="product-container has-shadow">
+                            <span className="product-label">AT&T Trek 2 HD</span>
                             <img src="" alt=""></img>
                         </div>
                         <Button href="#link" className='fn-primary'>Explore all tablets</Button>
@@ -61,20 +61,20 @@ export default class ShopPlansView extends React.Component {
                 <section className="product-row">
                     <div className="container">
                         <h1>In-Vehicle</h1>
-                        <div className="card-container has-shadow">
-                            <span></span>
+                        <div className="product-container has-shadow">
+                            <span className="product-label">AT&T Trek 2 HD</span>
                             <img src="" alt=""></img>
                         </div>
-                        <div className="card-container has-shadow">
-                            <span></span>
+                        <div className="product-container has-shadow">
+                            <span className="product-label">Cradlepoint IBR 1100</span>
                             <img src="" alt=""></img>
                         </div>
-                        <div className="card-container has-shadow">
-                            <span></span>
+                        <div className="product-container has-shadow">
+                            <span className="product-label">Sierra Wireless MG90 475x274</span>
                             <img src="" alt=""></img>
                         </div>
-                        <div className="card-container has-shadow">
-                            <span></span>
+                        <div className="product-container has-shadow">
+                            <span className="product-label">CalAmp Fusion</span>
                             <img src="" alt=""></img>
                         </div>
                         <Button href="#link" className='fn-primary'>Explore all In-Vehicle</Button>
@@ -83,20 +83,20 @@ export default class ShopPlansView extends React.Component {
                 <section className="product-row">
                     <div className="container">
                         <h1>Accessories</h1>
-                        <div className="card-container has-shadow">
-                            <span></span>
+                        <div className="product-container has-shadow">
+                            <span className="product-label">AT&T Unite Explore</span>
                             <img src="" alt=""></img>
                         </div>
-                        <div className="card-container has-shadow">
-                            <span></span>
+                        <div className="product-container has-shadow">
+                            <span className="product-label">GumDrop Marine Case</span>
                             <img src="" alt=""></img>
                         </div>
-                        <div className="card-container has-shadow">
-                            <span></span>
+                        <div className="product-container has-shadow">
+                            <span className="product-label">AudioVox Car<br></br>Connection 2.0</span>
                             <img src="" alt=""></img>
                         </div>
-                        <div className="card-container has-shadow">
-                            <span></span>
+                        <div className="product-container has-shadow">
+                            <span className="product-label">ZTE Mobley</span>
                             <img src="" alt=""></img>
                         </div>
                         <Button href="#link" className='fn-primary'>Explore all Accessories</Button>
