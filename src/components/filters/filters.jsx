8c4709bf1912825696e973c0262f31365d58f5d8
Justin Isaf <jisaf@sapient.com>
2017-05-01 16:49:58 -0400

    added correct route to down arrow. re-enabled category filter. updated segment filters. added, and commented out platform in case that's ever required. updated routes to use current services

--- src/components/filters/filters.jsx
+++ src/components/filters/filters.jsx
@@ -8,7 +8,7 @@ export class Filters extends React.Component {
     store: PropTypes.observableObject.isRequired
   }
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.store = this.props.store;
   }
@@ -21,33 +21,38 @@ export class Filters extends React.Component {
     this.store.changeSegmentFilter(event.target.value);
   }
 
+  handlePlatformChange = (event) => {
+    this.store.changePlatformFilter(event.target.value);
+  }
+
   render() {
     return (
-      <div>
-      <section className="filters">
-          <div className="row">
-            <div className="col-md-6">
-              <div className="form-group">
-                <span className="sr-only"><label htmlFor="category-filter">Category</label></span>
-                <select disabled id="category-filter" className="form-control" selected={this.store.categoryFilter} onChange={this.handleCategoryChange}>
-                  {this.store.categories.map((category, index) => {
-                    return <option value={category.value} key={index}>{category.title}</option>
-                  })}
-                </select>
-              </div>
-            </div>
-            <div className="col-md-6">
-              <div className="form-group">
-                <span className="sr-only"><label htmlFor="segment-filter">Segment</label></span>
-                  <select id="segment-filter" className="form-control" selected={this.store.segmentFilter} onChange={this.handleSegmentChange}>
-                    {this.store.segments.map((segment, index) => {
-                      return <option value={segment.value} key={index}>{segment.title}</option>
-                    })}
-                  </select>
-              </div>
-            </div>
-          </div>
-      </section>
+      <div className="filters row">
+        <div className="filter-title col-md-2">Filter</div>
+        <div className="form-group col-md-5 col-xs-12">
+          <span className="sr-only"><label htmlFor="category-filter">Category</label></span>
+          <select id="category-filter" className="form-control" selected={this.store.categoryFilter} onChange={this.handleCategoryChange}>
+            {this.store.categories.map((category, index) => {
+              return <option value={category.value} key={index}>{category.title}</option>
+            })}
+          </select>
+        </div>
+        <div className="form-group col-md-5 col-xs-12">
+          <span className="sr-only"><label htmlFor="segment-filter">Segment</label></span>
+          <select id="segment-filter" className="form-control" selected={this.store.segmentFilter} onChange={this.handleSegmentChange}>
+            {this.store.segments.map((segment, index) => {
+              return <option value={segment.value} key={index}>{segment.title}</option>
+            })}
+          </select>
+        </div>
+        {/*<div className="form-group col-md-3 hidden-xs hidden-sm">
+          <span className="sr-only"><label htmlFor="platform-filter">Platform</label></span>
+          <select id="platform-filter" className="form-control" selected={this.store.platformFilter} onChange={this.handlePlatformChange}>
+            {this.store.platforms.map((platform, index) => {
+              return <option value={platform.value} key={index}>{platform.title}</option>
+            })}
+          </select>
+        </div>*/}
       </div>
     );
   }
--- src/core/services/api.service.js
+++ src/core/services/api.service.js
@@ -8,12 +8,12 @@ const pseid = 'pse_id=123'
 
 class ApiService {
 
-    loadUserData() {
-        return axios.get('http://localhost:3000/user');
-    }
+    // loadUserData() {
+    //     return axios.get('http://localhost:3000/user');
+    // }
     
     getHomeCards() {
-        return axios.get(`${base}/apps/admin?${pseid}`)
+        return axios.get(`${base}/apps?${pseid}`)
             .then((res) => {
                 return utilsService.conditionData(res.data.applications);
             });
@@ -22,7 +22,7 @@ class ApiService {
     getSearchResults(query) {
         // TODO - swap routes when they are actually working
         // return axios.get(`${base}/apps/search?searchTxt=${query}&${pseid}`)
-        return axios.get(`${base}/apps/admin?${pseid}`)
+        return axios.get(`${base}/apps?${pseid}`)
             .then((res) => {
                 return utilsService.conditionData(res.data.applications);
             });
