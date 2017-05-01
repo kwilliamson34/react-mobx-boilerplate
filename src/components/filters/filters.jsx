00bef785fbe5b14ac6d18dcc833788c87751e341
Justin Isaf <jisaf@sapient.com>
2017-05-01 16:56:06 -0400

    update HTML to be more semantic, updated form.scss to conform better to standards

--- src/components/filters/filters.jsx
+++ src/components/filters/filters.jsx
@@ -21,38 +21,46 @@ export class Filters extends React.Component {
     this.store.changeSegmentFilter(event.target.value);
   }
 
-  handlePlatformChange = (event) => {
-    this.store.changePlatformFilter(event.target.value);
-  }
+  // handlePlatformChange = (event) => {
+  //   this.store.changePlatformFilter(event.target.value);
+  // }
 
   render() {
     return (
-      <div className="filters row">
-        <div className="filter-title col-md-2">Filter</div>
-        <div className="form-group col-md-5 col-xs-12">
-          <span className="sr-only"><label htmlFor="category-filter">Category</label></span>
-          <select id="category-filter" className="form-control" selected={this.store.categoryFilter} onChange={this.handleCategoryChange}>
-            {this.store.categories.map((category, index) => {
-              return <option value={category.value} key={index}>{category.title}</option>
-            })}
-          </select>
-        </div>
-        <div className="form-group col-md-5 col-xs-12">
-          <span className="sr-only"><label htmlFor="segment-filter">Segment</label></span>
-          <select id="segment-filter" className="form-control" selected={this.store.segmentFilter} onChange={this.handleSegmentChange}>
-            {this.store.segments.map((segment, index) => {
-              return <option value={segment.value} key={index}>{segment.title}</option>
-            })}
-          </select>
-        </div>
-        {/*<div className="form-group col-md-3 hidden-xs hidden-sm">
-          <span className="sr-only"><label htmlFor="platform-filter">Platform</label></span>
-          <select id="platform-filter" className="form-control" selected={this.store.platformFilter} onChange={this.handlePlatformChange}>
-            {this.store.platforms.map((platform, index) => {
-              return <option value={platform.value} key={index}>{platform.title}</option>
-            })}
-          </select>
-        </div>*/}
+      <div>
+        <section className="filters">
+          <div className="row">
+            <div className="filter-title col-md-2">Filter</div>
+            <div className="col-md-5 col-xs-12">
+              <div className="form-group">
+                <span className="sr-only"><label htmlFor="category-filter">Category</label></span>
+                <select id="category-filter" className="form-control" selected={this.store.categoryFilter} onChange={this.handleCategoryChange}>
+                  {this.store.categories.map((category, index) => {
+                    return <option value={category.value} key={index}>{category.title}</option>
+                  })}
+                </select>
+              </div>
+            </div>
+            <div className="col-md-5 col-xs-12">
+              <div className="form-group">
+                <span className="sr-only"><label htmlFor="segment-filter">Segment</label></span>
+                <select id="segment-filter" className="form-control" selected={this.store.segmentFilter} onChange={this.handleSegmentChange}>
+                  {this.store.segments.map((segment, index) => {
+                    return <option value={segment.value} key={index}>{segment.title}</option>
+                  })}
+                </select>
+              </div>
+            </div>
+            {/*<div className="form-group col-md-3 hidden-xs hidden-sm">
+              <span className="sr-only"><label htmlFor="platform-filter">Platform</label></span>
+              <select id="platform-filter" className="form-control" selected={this.store.platformFilter} onChange={this.handlePlatformChange}>
+                {this.store.platforms.map((platform, index) => {
+                  return <option value={platform.value} key={index}>{platform.title}</option>
+                })}
+              </select>
+            </div>*/}
+          </div>
+        </section>
       </div>
     );
   }
