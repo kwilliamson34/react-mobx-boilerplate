06ef33a093287edd5e6e487d07a70ad06d3e50c9
Nassir Isaf <njisaf@gmail.com>
2017-05-03 17:19:50 -0400

    Disastrous refactor reversed. Back to square one with styling. Question is how to make the figures stick on the bottom, the rest is just slap and tickle.

--- src/components/screenshot-gallery/screenshot-gallery.jsx
+++ src/components/screenshot-gallery/screenshot-gallery.jsx
@@ -29,56 +29,69 @@ export class ScreenshotGallery extends React.Component {
   render() {
     let hideGalleryIfNoShots = this.screenshots.length === 0 ? {display: 'none'} : {};
 
-    let slides = [];
-    let captions = [];
-
-    this.screenshots.map((node, i) => {
-      slides.push(
-        <div key={i} className="slide-container container-fluid">
-          <figure className='slide-figure img-responsive'>
-            <img src={'/images/' + node.path} className='slide-img' alt={'Image for ' + node.description} />
-          </figure>
-        </div>
-      )
-      captions.push(
-        <figcaption key={i} className='slide-caption'>{node.description}</figcaption>
+    let slides = this.screenshots.map((node, i) => {
+      return (
+        <Slide key={i} path={node.path} caption={node.description} />
       )
     });
 
     return (
       <div className="gallery-container" style={hideGalleryIfNoShots}>
-        <div className="slide-container">
-          {slides}
-        </div>
-        <div className="caption-container">
-          {captions}
-        </div>
+        {slides}
       </div>
     )
   }
 }
 
-// class Slide extends React.Component {
+class Slide extends React.Component {
+
+  static propTypes = {
+    description: PropTypes.string,
+    path: PropTypes.string
+  }
+
+  static defaultProps = {
+    description: '',
+    path: '../../images/app-icon.png'
+  }
+
+  render() {
+
+    return (
+      <div className="slide-container container-fluid">
+        <figure className='slide-figure img-responsive'>
+          <img src={'/images/' + this.props.path} className='slide-img' alt={'Image for ' + this.props.description} />
+        </figure>
+        <figcaption className='slide-caption'>{this.props.description}</figcaption>
+      </div>
+    )
+  }
+}
+
+
+// this.screenshots.map((node, i) => {
+//   slides.push(
+//     <div key={i} className="slide-container container-fluid">
+//       <figure className='slide-figure img-responsive'>
+//         <img src={'/images/' + node.path} className='slide-img' alt={'Image for ' + node.description} />
+//       </figure>
+//     </div>
+//   )
+//   captions.push(
+//     <figcaption key={i} className='slide-caption'>{node.description}</figcaption>
+//   )
+// });
 //
-//   static propTypes = {
-//     description: PropTypes.string,
-//     path: PropTypes.string
-//   }
-//
-//   static defaultProps = {
-//     description: '',
-//     path: '../../images/app-icon.png'
-//   }
-//
-//   render() {
-//
-//     return (
-//       <div className="slide-container container-fluid">
-//         <figure className='slide-figure img-responsive'>
-//           <img src={'/images/' + this.props.path} className='slide-img' alt={'Image for ' + this.props.description} />
-//         </figure>
-//         <figcaption className='slide-caption'>{this.props.description}</figcaption>
+// return (
+//   <div className="gallery-container" style={hideGalleryIfNoShots}>
+//     <div className="inner-flex-container">
+//       <div className="slide-container">
+//         {slides}
 //       </div>
-//     )
-//   }
+//       <div className="caption-container">
+//         {captions}
+//       </div>
+//     </div>
+//   </div>
+// )
 // }
