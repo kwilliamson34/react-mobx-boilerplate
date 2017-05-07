23c517f4b5675c9be54fd812e663c6cceb989843
Nassir Isaf <njisaf@gmail.com>
2017-05-07 18:05:02 -0400

    not sure what's next

--- src/components/screenshot-gallery/__tests__/screenshot-gallery.test.js
+++ src/components/screenshot-gallery/__tests__/screenshot-gallery.test.js
@@ -4,14 +4,48 @@ import ScreenshotGallery from '../screenshot-gallery';
 
 describe('<ScreenshotGallery />', () => {
 
-  let props = {
-    screenshots: [
+  let props = [
       {
         description: "Image1",
         path: "nji-SurfacePro4-mock.png"
+      },
+      {
+        description: "Image2",
+        path: "nji-iPhone6s-mock.png"
+      },
+      {
+        description: "Image3",
+        path: "nji-Android1080p-mock.png"
+      },
+      {
+        description: "Image4",
+        path: "nji-SurfacePro4-mock.png"
+      },
+      {
+        description: "Image5",
+        path: "nji-iPhone6s-mock.png"
+      },
+      {
+        description: "Image6",
+        path: "nji-iPhone6s-mock.png"
+      },
+      {
+        description: "Image7",
+        path: "nji-SurfacePro4-mock.png"
+      },
+      {
+        description: "Image8",
+        path: "nji-SurfacePro4-mock.png"
+      },
+      {
+        description: "Image9",
+        path: "nji-Android1080p-mock.png"
+      },
+      {
+        description: "Image10",
+        path: "nji-SurfacePro4-mock.png"
       }
-    ]
-  };
+    ];
 
 
   describe('it should render', () => {
