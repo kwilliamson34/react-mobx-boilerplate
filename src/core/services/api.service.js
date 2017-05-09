cf98858fce7695abd74c7bbe3c7afae7bf67fe15
Paul Arce <parce@sapient.com>
2017-05-09 15:00:08 -0400

    making sure pseId is used instead of pse_id for api service calls

--- src/core/services/api.service.js
+++ src/core/services/api.service.js
@@ -4,7 +4,7 @@ import { utilsService } from './utils.service';
 const base = '/api'
 
 // TODO - temp hardcode pending PSEID implementation
-const pseid = 'pse_id=123'
+const pseid = 'pseId=123'
 
 class ApiService {
 
