e8608232939052d4a8d5b7fe6405d7e7a3c6f564
Nassir Isaf <njisaf@gmail.com>
2017-05-11 16:52:12 -0400

    forgot to save

--- src/components/app-reviews/__tests__/app-reviews.test.js
+++ src/components/app-reviews/__tests__/app-reviews.test.js
@@ -8,9 +8,9 @@ describe('<AppReviews /> with one item;', () => {
   let props = {
     reviews: [
       {
-        subject: 'Review Subject',
-        author: 'Review Author',
-        rating: 5,
+        subject: '',
+        author: '',
+        rating: 0,
         date: 'December 23rd, 2012',
         comment: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
       }
@@ -41,12 +41,33 @@ describe('<AppReviews /> with many items;', () => {
       },
       {
         subject: 'Review Subject 2',
-        author: 'Review Author 3',
+        author: 'Review Author 2',
         rating: 4,
         date: 'December 24rd, 2012',
         comment: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+      },
+      {
+        subject: 'Review Subject 3',
+        author: 'Review Author 3',
+        rating: 4,
+        date: 'December 25rd, 2012',
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+      },
+      {
+        subject: 'Review Subject 4',
+        author: 'Review Author 4',
+        rating: 4,
+        date: 'December 26rd, 2012',
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+      },
+      {
+        subject: 'Review Subject 5',
+        author: 'Review Author 5',
+        rating: 4,
+        date: 'December 27rd, 2012',
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
       }
-  ]
+    ]
   };
 
   describe('It should render;', () => {
