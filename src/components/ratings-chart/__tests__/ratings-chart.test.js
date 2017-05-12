3dfe9b58b0c9d95b591f36672f8547ca28226eb1
Paul Arce <parce@sapient.com>
2017-05-12 11:22:33 -0400

    updated testing and committing snapshots for ratings component

--- src/components/ratings-chart/__tests__/ratings-chart.test.js
+++ src/components/ratings-chart/__tests__/ratings-chart.test.js
@@ -20,5 +20,35 @@ describe('<RatingsChart />', () => {
           let tree = component.toJSON();
           expect(tree).toMatchSnapshot();
         });
+        test('matches snapshot for ratings-chart when only one review is passed in', () => {
+          let props = {
+              value: 3.0,
+              reviewsTotal: 1,
+              data: [0, 0, 1, 0, 0]
+          };
+
+          const component = renderer.create(
+              <MemoryRouter>
+                  <RatingsChart {...props} />
+              </MemoryRouter>
+          );
+          let tree = component.toJSON();
+          expect(tree).toMatchSnapshot();
+        });
+        test('matches snapshot for ratings-chart when zeroes are passed in', () => {
+          let props = {
+              value: 0,
+              reviewsTotal: 0,
+              data: [0, 0, 0, 0, 0]
+          };
+
+          const component = renderer.create(
+              <MemoryRouter>
+                  <RatingsChart {...props} />
+              </MemoryRouter>
+          );
+          let tree = component.toJSON();
+          expect(tree).toMatchSnapshot();
+        });
     });
 });
