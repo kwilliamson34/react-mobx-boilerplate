3dff569b1231ab1e5bee90800e31a9012a140b8b
Katy Williamson <kwilliamson@sapient.com>
2017-05-11 20:12:48 -0400

    beautifying the new geolink.store

--- src/core/stores/geolink.store.js
+++ src/core/stores/geolink.store.js
@@ -1,89 +1,92 @@
-import { action, observable } from 'mobx';
+import {
+  action,
+  observable
+} from 'mobx';
 
 const networkLayerNames = [
-	'FirstNet:Coverage2G',
-	'FirstNet:Coverage3G4GIndoor',
-	'FirstNet:Coverage3G4GOutdoor',
-	'FirstNet:CoverageLTEWithPriorityIndoor',
-	'FirstNet:CoverageLTEWithPriorityOutdoor',
-	'FirstNet:CoverageLTEWithoutPriority'
+  'FirstNet:Coverage2G',
+  'FirstNet:Coverage3G4GIndoor',
+  'FirstNet:Coverage3G4GOutdoor',
+  'FirstNet:CoverageLTEWithPriorityIndoor',
+  'FirstNet:CoverageLTEWithPriorityOutdoor',
+  'FirstNet:CoverageLTEWithoutPriority'
 ];
 // const outageLayerName = 'FirstNet:GTOCOutages';
 
 class GeolinkStore {
 
-	@action addAllCoverageLayers() {
-		networkLayerNames.map(layerName => {
-			this.addLayer(layerName);
-		});
-	}
+  @action addAllCoverageLayers() {
+    networkLayerNames.map(layerName => {
+      this.addLayer(layerName);
+    });
+  }
 
-	@action removeAllCoverageLayers() {
-		networkLayerNames.map(layerName => {
-			this.removeLayer(layerName);
-		});
-	}
+  @action removeAllCoverageLayers() {
+    networkLayerNames.map(layerName => {
+      this.removeLayer(layerName);
+    });
+  }
 
-	@action addLayer(layerName) {
-		console.log('Turning ON ' + layerName);
+  @action addLayer(layerName) {
+    console.log('Turning ON ' + layerName);
     this.mapIframeRef.contentWindow.postMessage({
       eventName: 'loadLayer',
       value: layerName,
       flag: true
     }, '*');
-	}
+  }
 
-	@action removeLayer(layerName) {
-		console.log('Turning OFF ' + layerName);
+  @action removeLayer(layerName) {
+    console.log('Turning OFF ' + layerName);
     this.mapIframeRef.contentWindow.postMessage({
       eventName: 'loadLayer',
       value: layerName,
       flag: false
     }, '*');
-	}
+  }
 
-	@action updateSearchTerm(searchTerm){
-		this.searchTerm = searchTerm;
-	}
+  @action updateSearchTerm(searchTerm) {
+    this.searchTerm = searchTerm;
+  }
 
-	@action searchMap(){
-		console.log('Searching map for ' + this.searchTerm + '...');
+  @action searchMap() {
+    console.log('Searching map for ' + this.searchTerm + '...');
     this.mapIframeRef.contentWindow.postMessage({
       eventName: 'doMapGeocode',
       value: this.searchTerm
     }, '*');
-	}
+  }
 
-	@action toggleTraffic() {
-		console.log('Toggling traffic');
+  @action toggleTraffic() {
+    console.log('Toggling traffic');
     this.mapIframeRef.contentWindow.postMessage({
       eventName: 'toggleTraffic'
     }, '*');
-	}
+  }
 
-	@action addWeather() {
-		console.log('Adding weather layer/animation');
-		const options = {
-			opacity: .7
-		};
-		this.mapIframeRef.contentWindow.postMessage({
+  @action addWeather() {
+    console.log('Adding weather layer/animation');
+    const options = {
+      opacity: .7
+    };
+    this.mapIframeRef.contentWindow.postMessage({
       eventName: 'loadRadar',
-			options: options
+      options: options
     }, '*');
-		/*TODO Evaluate weather the "animate weather" event is needed
-		once the map is visible and we know what the baseline weather
-		display is. */
-	}
+    /*TODO Evaluate weather the "animate weather" event is needed
+    once the map is visible and we know what the baseline weather
+    display is. */
+  }
 
-	@action removeWeather() {
-		console.log('Removing weather layer/animation');
+  @action removeWeather() {
+    console.log('Removing weather layer/animation');
     this.mapIframeRef.contentWindow.postMessage({
       eventName: 'removeRadar'
     }, '*');
-	}
+  }
 
-	@observable mapIframeRef = null;
-	@observable searchTerm = '';
+  @observable mapIframeRef = null;
+  @observable searchTerm = '';
 
 }
 
