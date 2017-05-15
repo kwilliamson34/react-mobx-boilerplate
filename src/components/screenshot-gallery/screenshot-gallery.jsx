ec4c3b31efa682ecf136c89f33279bb03a5b76f0
Nassir Isaf <njisaf@gmail.com>
2017-05-15 13:38:39 -0400

    pulling in work from screenshots

---
+++ src/components/screenshot-gallery/screenshot-gallery.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { observer } from 'mobx-react';
+
+@observer
+export default class ScreenshotGallery extends React.Component {
+
+  static propTypes = {
+    screenshots: PropTypes.shape({
+      mobile: PropTypes.observableArray,
+      tablet: PropTypes.observableArray
+    })
+  }
+
+  static defaultProps = {
+    screenshots: {
+      mobile: [],
+      tablet: []
+    }
+  }
+
+
+  renderSlides = (screenshots) => {
+    let screenshotArray = [...screenshots.mobile, ...screenshots.tablet];
+    return screenshotArray.map((node, i) => {
+      return (
+        <div key={i} className='slide-container'>
+          <figure className='img-responsive'>
+            <img src={node.path} className='slide-img' alt={'Image for ' + node.description} aria-labelledby={'slide-caption-' + i} />
+          </figure>
+          <figcaption className='slide-caption' id={'slide-caption-' + i}>{node.description}</figcaption>
+        </div>
+      )
+    }
+  )};
+
+  render() {
+    return (
+      <div className='gallery-container' role='region' aria-label='App screenshot gallery'>
+        {this.renderSlides(this.props.screenshots)}
+      </div>
+    )
+  }
+}
