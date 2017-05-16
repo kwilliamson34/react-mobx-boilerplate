a13db5e8378908e5eb0600dfc6dad27230f2d13b
Nassir Isaf <njisaf@gmail.com>
2017-05-16 14:30:17 -0400

    cleaning up appReviews that got pulled in

--- src/pages/app-details.page.jsx
+++ src/pages/app-details.page.jsx
@@ -11,7 +11,6 @@ import {
 } from '../components/rating/rating';
 import RatingsChart from '../components/ratings-chart/ratings-chart';
 import Toggle from '../components/toggle/toggle';
-import AppReviews from '../components/app-reviews/app-reviews';
 import ScreenshotGallery from '../components/screenshot-gallery/screenshot-gallery';
 
 //import mock response from services
@@ -124,10 +123,6 @@ export default class AppDetailsPage extends React.Component {
               <div className="col-xs-12 col-sm-12 col-md-offset-1 col-md-10 col-lg-offset-1 col-lg-10">
                 <h2>Reviews</h2>
 								<RatingsChart value={4.1} reviewsTotal={44} data={[14,22,8,5,2]}/>
-								{ appDetail.reviews.length === 0
-									? <div className="content-description">No reviews yet!</div>
-									: <AppReviews reviews={appDetail.reviews} />
-								}
 							</div>
 						</div>
           </div>
