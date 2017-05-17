97972ec299d877d6b80a79dd9fe6a2424e6ce6dc
Paul Arce <parce@sapient.com>
2017-05-17 09:47:38 -0400

    ripped out react bootstrap and updated PropTypes import to be from npm module instead of react.

--- src/components/geolink-map/geolink-layer-toggle.jsx
+++ src/components/geolink-map/geolink-layer-toggle.jsx
@@ -1,29 +1,30 @@
-import React, {PropTypes} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class GeolinkLayerToggle extends React.Component {
-  static propTypes = {
-    value: PropTypes.string.isRequired,
-    label: PropTypes.string.isRequired,
-    onClick: PropTypes.func,
-    defaultOn: PropTypes.bool
-  }
+	static propTypes = {
+		value: PropTypes.string.isRequired,
+		label: PropTypes.string.isRequired,
+		onClick: PropTypes.func,
+		defaultOn: PropTypes.bool
+	}
 
-  constructor(props) {
-    super(props);
-    if(this.props.onClick){
-      this.onClick = this.props.onClick.bind(this);
-    }
-  }
+	constructor(props) {
+		super(props);
+		if (this.props.onClick) {
+			this.onClick = this.props.onClick.bind(this);
+		}
+	}
 
-  render() {
-    return (
-      <div className="checkbox">
+	render() {
+		return (
+			<div className="checkbox">
         <label>
           <input type="checkbox" value={this.props.value} defaultChecked={this.props.defaultOn} onClick={this.onClick}/>
           <span className="cr"></span>
           <span>{this.props.label}</span>
         </label>
       </div>
-    )
-  }
+		)
+	}
 }
