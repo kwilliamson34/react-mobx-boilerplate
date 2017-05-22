2f2e8bae97f413c3950900eed86739c8c11bd79a
Paul Arce <parce@sapient.com>
2017-05-22 09:22:51 -0400

    adding active class for iconItems

--- src/pages/help-center.page.jsx
+++ src/pages/help-center.page.jsx
@@ -6,10 +6,15 @@ export default class HelpCenterPage extends React.Component {
   render() {
     return (
       <article id="help-center-page">
-        <section className="placeholder-long">
-            <div className="container">
-                <h1 className="as-h2">Help Center</h1>
-            </div>
+        <section className="content-wrapper">
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-12"><h1 className="as-h2">Help Center</h1></div>
+          </div>
+          <div className="row">
+            <div className="col-xs-12"><p>Content Here</p></div>
+          </div>
+        </div>
         </section>
       </article>
     )
