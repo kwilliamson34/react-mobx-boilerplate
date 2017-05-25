0f0717a397aea6408ab939d905f662d50ad7ca34
Katy Williamson <kwilliamson@sapient.com>
2017-05-25 10:34:48 -0400

    initial commit of NewTabLink usage on admin dashboard

--- src/pages/admin-dashboard.page.jsx
+++ src/pages/admin-dashboard.page.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import {
-	Link
-} from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import NewTabLink from '../components/link/new-tab-link';
 
 export default class AdminDashboardPage extends React.Component {
+	constructor(props){
+		super(props);
+		this.showPushToTalkModal = this.showPushToTalkModal.bind(this);
+	}
+
+	showPushToTalkModal() {
+		//TODO launch the modal to choose push-to-talk provider
+	}
+
 	render() {
 		return (
 			<article id="admin-dashboard-page">
@@ -19,31 +27,31 @@ export default class AdminDashboardPage extends React.Component {
 								<nav>
 									<ul>
 										<li className="col-xs-12 col-sm-6">
-											<Link to="/admin/manage-users" className="dashboard-card manage-users has-shadow">
+											<NewTabLink to="https://profilemgt.firstnet.att.com/ebiz/firstnet/index.jsp" className="dashboard-card manage-users has-shadow">
 												<div className="desc">
 													<h3>Manage users</h3>
 													<p>Add, edit and remove users.</p>
 												</div>
 												<span>Manage Users</span>
-											</Link>
+											</NewTabLink>
 										</li>
 										<li className="col-xs-12 col-sm-6">
-											<Link to="/admin/manage-billing" className="dashboard-card manage-billing has-shadow">
+											<NewTabLink to="TODO" className="dashboard-card manage-billing has-shadow">
 												<div className="desc">
 													<h3>Manage billing</h3>
 													<p>View &amp; pay bills, update info</p>
 												</div>
 												<span>Manage Billing</span>
-											</Link>
+											</NewTabLink>
 										</li>
 										<li  className="col-xs-12 col-sm-6">
-											<Link to="/admin/manage-services" className="dashboard-card manage-services has-shadow">
+											<NewTabLink to="https://wireless.firstnet.att.com/b2bservlets/HaloSSOLoginServlet.dyn" className="dashboard-card manage-services has-shadow">
 												<div className="desc">
 													<h3>Manage services</h3>
 													<p>Assign or remove device, change rate plans &amp; features</p>
 												</div>
 												<span>Manage Services</span>
-											</Link>
+											</NewTabLink>
 										</li>
 										<li className="col-xs-12 col-sm-6">
 											<Link to="/admin/manage-apps" className="dashboard-card manage-apps has-shadow">
@@ -55,22 +63,22 @@ export default class AdminDashboardPage extends React.Component {
 											</Link>
 										</li>
 										<li className="col-xs-12 col-sm-6">
-											<Link to="/admin/manage-push-talk" className="dashboard-card manage-push-talk has-shadow">
+											<button className="dashboard-card manage-push-talk has-shadow as-link" onClick={this.showPushToTalkModal}>
 												<div className="desc">
 													<h3>Manage push-to-talk</h3>
 													<p>Create and remove talk groups, add and remove users from talk groups</p>
 												</div>
 												<span>Manage Push-to-Talk</span>
-											</Link>
+											</button>
 										</li>
 										<li className="col-xs-12 col-sm-6">
-											<Link to="/admin/view-wireless-reports" className="dashboard-card manage-wireless-reports has-shadow">
+											<NewTabLink to="https://www.wireless.att.com/businesscare/menu/index.jsp?subject=Reports&wtLinkName=Reports&wtLinkLoc=S1&&wtLinkType=InventoryReport" className="dashboard-card manage-wireless-reports has-shadow">
 												<div className="desc">
 													<h3>View wireless reports</h3>
 													<p>View device inventory, rate plan summary, early termination fees, upgrade eligibility, device unlock eligibility</p>
 												</div>
 												<span>View Wireless Reports</span>
-											</Link>
+											</NewTabLink>
 										</li>
 									</ul>
 								</nav>
@@ -82,13 +90,13 @@ export default class AdminDashboardPage extends React.Component {
 							<nav>
 								<ul>
 									<li className="col-xs-12 col-md-6 col-lg-12">
-										<Link to="/shop-devices-rates" className="dashboard-card shop-devices-rates has-shadow">
+										<NewTabLink to="TODO" className="dashboard-card shop-devices-rates has-shadow">
 											<div className="desc">
 												<h3>Shop standard devices &amp; rate plans</h3>
 												<p>Add a new device, provision an existing device, add a rate plan, feature(s) and accessories</p>
 											</div>
 											<span>Shop Devices &amp; Plans</span>
-										</Link>
+										</NewTabLink>
 									</li>
 									<li className="col-xs-12 col-md-6 col-lg-12">
 										<Link to="/shop-specialized-devices" className="dashboard-card shop-specialized-devices has-shadow">
