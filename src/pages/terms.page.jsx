6e5332755544bba51d16376641416e0adb17bc70
Paul Arce <parce@sapient.com>
2017-05-25 12:21:34 -0400

    FPSE-399 updates to PSE homepage footer * now employs the header store, replacing internal state

---
+++ src/pages/terms.page.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default class TermsOfServicePage extends React.Component {
+  render() {
+    return (
+      <article id="help-center-page">
+        <section className="content-wrapper">
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-12"><h1 className="as-h2">Terms of Service</h1></div>
+          </div>
+          <div className="row">
+            <div className="col-xs-12"><p>Content Here</p></div>
+          </div>
+        </div>
+        </section>
+      </article>
+    )
+  }
+}
