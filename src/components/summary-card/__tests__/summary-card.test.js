fd5369ed9cb41743ff0197690eb6d167ddee7d49
Katy Williamson <kwilliamson@sapient.com>
2017-05-26 12:58:09 -0400

    removing unecessary prop copy on summary card; fixing unit test

--- src/components/summary-card/__tests__/summary-card.test.js
+++ src/components/summary-card/__tests__/summary-card.test.js
@@ -2,6 +2,7 @@ jest.unmock('../summary-card');
 jest.unmock('../../rating/rating');
 
 import {SummaryCard} from '../summary-card';
+import {MemoryRouter} from 'react-router-dom';
 
 describe('<SummaryCard />', () => {
   let props = {
@@ -17,35 +18,24 @@ describe('<SummaryCard />', () => {
 
   describe('render testing', () => {
     test('matches snapshot', () => {
-      const component = renderer.create(<SummaryCard {...props} />);
+      let component = renderer.create(
+        <MemoryRouter>
+          <SummaryCard {...props} />
+        </MemoryRouter>
+      );
       let tree = component.toJSON();
       expect(tree).toMatchSnapshot();
-    });
-  });
 
-  describe('componentWillReceiveProps testing', () => {
-    let node, component;
-    let newProps = {
-      display:{
-        name: 'new app name',
-        publisher: 'new publisher name',
-        imageUrl: '../../images/app-icon.png',
-        rating: 1.5,
-        badge: false,
-        operatingSystem: 'ANDROID',
-      }
-    };
-
-    // render the original component
-    beforeEach(function(){
-        node = document.createElement('div');
-        component = ReactDOM.render(<SummaryCard {...props} />, node);
+      props.display.rating = 1.5;
+      props.display.badge = false;
+      props.display.operatingSystem = 'IOS';
+      component = renderer.create(
+        <MemoryRouter>
+          <SummaryCard {...props} />
+        </MemoryRouter>
+      );
+      tree = component.toJSON();
+      expect(tree).toMatchSnapshot();
     });
-    test('should update the state of the component when the value prop is changed', () => {
-    // `component` will be updated instead of remounted
-    ReactDOM.render(<SummaryCard {...newProps} />, node);
-    // Assert that `component` has updated its state in response to a prop change
-    expect(component.display.name).toBe('new app name');
-});
   });
 });
