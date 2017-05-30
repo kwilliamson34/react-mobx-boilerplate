333dc892c4071c8a0c91e0c4ddc888d1d2d4efb7
mlgantt <mgantt@sapient.com>
2017-05-30 16:42:50 -0400

    added error code check for 401

--- src/core/stores/user.store.js
+++ src/core/stores/user.store.js
@@ -11,7 +11,7 @@ class UserStore {
             this.checkPermissions();
         }
         const fail = (err) => {
-            if(err.response.status === 403) {
+            if(err.response.status === 403 || err.response.status === 401) {
                 window.location.replace('https://oidc.stage.flogin.att.com/isam/oidc/endpoint/amapp-runtime-SSPRS/authorize?response_type=id_token+token&client_id=m11635&state=FWpMHzl61gXfcnMmwkp4&&scope=openid&nonce=dsZHN5kvm2a4cVIA0ZdN&response_mode=form_post');
             } else {
                 console.warn(err);
