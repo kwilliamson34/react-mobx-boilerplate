408d0cbde3ffd9048ed87782944457e06af726df
Katy Williamson <kwilliamson@sapient.com>
2017-06-02 13:06:59 -0400

    adding error handler to all stores; updating error page; handling JWT token timeout

--- src/core/services/utils.service.js
+++ src/core/services/utils.service.js
@@ -1,44 +1,58 @@
 import config from 'config';
 
 class UtilsService {
-	conditionData(objs) {
-		let simplifiedObjs = objs.filter((obj) => {
-			if (obj.custom_metadata === null || (obj.custom_metadata && (obj.custom_metadata.user_segment === null || (obj.custom_metadata.user_segment && obj.custom_metadata.user_segment.length < 1)))) {
-					return false;
-				} else {
-					return true;
-				}
-			}).map((obj) => {
-					let simplifiedObj = {
-						name: obj.app_name,
-						publisher: obj.author,
-						imageUrl: config.apperianUploads + obj.icon_path,
-						rating: obj.rating,
-						id: obj.id,
-						app_psk: obj.app_psk,
-						isAvailable: obj.isAvailable,
-						isRecommended: obj.isRecommended,
-						badge: obj.is_endorsed,
-						operatingSystem: obj.operating_system,
-						category: [],
-						user_segment: [],
-						screenshots: {
-							mobile: [],
-							tablet: []
-						},
-						reviews: obj.reviews
-					}
-					if (obj.custom_metadata) {
-						simplifiedObj.category = obj.custom_metadata.category;
-						simplifiedObj.user_segment = obj.custom_metadata.user_segment;
-					}
+  conditionData(objs) {
+    let simplifiedObjectList = objs.filter(obj => {
+      //only process objects that have valid metadata
+      return obj.custom_metadata && obj.custom_metadata.user_segment && obj.custom_metadata.user_segment.length > 0;
+    }).map(obj => {
+      return {
+        name: obj.app_name,
+        publisher: obj.author,
+        imageUrl: config.apperianUploads + obj.icon_path,
+        rating: obj.rating,
+        id: obj.id,
+        app_psk: obj.app_psk,
+        isAvailable: obj.isAvailable,
+        isRecommended: obj.isRecommended,
+        badge: obj.is_endorsed,
+        operatingSystem: obj.operating_system,
+        category: obj.custom_metadata.category,
+        user_segment: obj.custom_metadata.user_segment,
+        screenshots: {
+          mobile: [],
+          tablet: []
+        }
+      }
+    });
+    return simplifiedObjectList;
+  }
 
-					return simplifiedObj;
-				})
-
-			return simplifiedObjs;
-	}
-
+  handleError(err) {
+    switch (err.response.status) {
+      case 401:
+        console.log('Received 401 Unauthenticated response, redirecting to Halo...');
+        window.location.replace(config.haloLogin);
+        break;
+      case 403:
+        console.log('Received 403 Forbidden response, redirecting to Halo...');
+        //TODO design and build an Unauthorized error page, redirect there using react router
+        window.location.replace(config.haloLogin);
+        break;
+      case 500:
+        if (err.message.indexOf('Bearer token is invalid' > -1)) {
+          console.log('Received 500 Internal Error response: Bearer token invalid, redirecting to Halo...');
+          window.location.replace(config.haloLogin);
+        } else {
+          console.warn(err);
+        }
+        break;
+      default:
+        console.log('Received ' + err.response.status + ' response, no special handling required.');
+        console.warn(err);
+        break;
+    }
+  }
 }
 
 export const utilsService = new UtilsService();
