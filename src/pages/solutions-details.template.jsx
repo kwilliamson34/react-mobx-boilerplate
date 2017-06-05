896e3b5f3ab007c7077cf7b2f33058a6602e629f
Nassir Isaf <njisaf@gmail.com>
2017-06-05 12:03:49 -0400

    elements on page. now to style

--- src/pages/solutions-details.template.jsx
+++ src/pages/solutions-details.template.jsx
@@ -8,8 +8,24 @@ const mockDetailPage = {
   description: 'A solution for many of the problems faced by your industry and your community. Works well in critical situations, reliable at all times. Rugged designs, hardened interfaces. Waterproof at literally any depth of water. Unaffected by temperature and humidity. Certified impervious to dogs, crows, wild swine, and small varmints. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati ut, nemo sunt eligendi numquam amet, aliquam dolor, distinctio itaque alias, quos ducimus. Voluptatem totam, iure fugiat modi exercitationem nemo ipsam.',
   headerPath: 'https://www-test.firstnet.ws/sites/default/files/images/content/banner/Tools_0.png',
   imagePath: 'https://www-test.firstnet.ws/sites/default/files/images/blocks/4.Solutions_Tools%20Mobile%20Forms%201008.jpg',
-  caption: 'Emergency responders using the solution.'
+  caption: 'Emergency responders using the solution.',
+  purchasing: {
+   contact: 'Lucius Fox',
+   phone: '800-XXX-XXX',
+   email: 'lfox@samsung.com',
+   company: 'Samsung Group',
+   url: 'https://www.samsung.com/us'
+ }
 }
+
+// const mockDetailPage = {
+//   title: 'Advanced Solution',
+//   description: 'A solution for many of the problems faced by your industry and your community. Works well in critical situations, reliable at all times. Rugged designs, hardened interfaces. Waterproof at literally any depth of water. Unaffected by temperature and humidity. Certified impervious to dogs, crows, wild swine, and small varmints. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati ut, nemo sunt eligendi numquam amet, aliquam dolor, distinctio itaque alias, quos ducimus. Voluptatem totam, iure fugiat modi exercitationem nemo ipsam.',
+//   headerPath: 'https://www-test.firstnet.ws/sites/default/files/images/content/banner/Tools_0.png',
+//   imagePath: 'https://www-test.firstnet.ws/sites/default/files/images/blocks/4.Solutions_Tools%20Mobile%20Forms%201008.jpg',
+//   caption: 'Emergency responders using the solution.',
+//   purchasing: {}
+// }
 
 @inject('store')
 @observer
@@ -22,15 +38,43 @@ export default class SolutionsDetailsTemplate extends React.Component {
 
   constructor(props) {
     super(props);
-    this.externalStore = this.props.store.externalLinkStore;
+    this.renderPurchasingInfo = this.renderPurchasingInfo.bind(this);
   }
 
-  componentWillMount() {
-    // this.externalStore.getSolutionDetails(this.props.match.url);
-    // this.externalStore.getSolutionHeaderImg(this.props.match.url);
+  renderPurchasingInfo() {
+    return (
+      <section className="purchasing-info">
+        <h3>For Purchasing</h3>
+        <dl role="list">
+          <dt>Contact:</dt>
+          <dd>
+            {mockDetailPage.purchasing.contact}
+          </dd>
+          <dt>Phone:</dt>
+          <dd>
+            <a href={`tel:+1${mockDetailPage.purchasing.phone}`}>{mockDetailPage.purchasing.phone}</a>
+          </dd>
+          <dt>Email:</dt>
+          <dd>
+            <a href={`mailto:${mockDetailPage.purchasing.email}`}>{mockDetailPage.purchasing.email}</a>
+          </dd>
+          <dt>Company:</dt>
+          <dd>
+            <a href={mockDetailPage.purchasing.url}>{mockDetailPage.purchasing.company}</a>
+          </dd>
+          <dt>Website:</dt>
+          <dd>
+            <a href={mockDetailPage.purchasing.url}>{mockDetailPage.purchasing.url.split('www.')[1]}</a>
+          </dd>
+        </dl>
+      </section>
+    )
   }
 
   render() {
+
+    //TODO: Purchasing info section should not render if no info is available, but we don't know what that will look like. This assumes there will be an empty object, but it may need to be revised.
+    const shouldRenderPurchasingInfo = Object.keys(mockDetailPage.purchasing).length !== 0;
 
     return (
       <article id="shop-solutions-page">
@@ -45,10 +89,7 @@ export default class SolutionsDetailsTemplate extends React.Component {
               <img src={mockDetailPage.imagePath} alt={"Image for " + mockDetailPage.title}/>
               <figcaption>{mockDetailPage.caption}</figcaption>
             </figure>
-            <section className="purchasing-info">
-              <h3>For Purchasing</h3>
-
-            </section>
+            {shouldRenderPurchasingInfo && this.renderPurchasingInfo()}
           </div>
         </div>
         </section>
