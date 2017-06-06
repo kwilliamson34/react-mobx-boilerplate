700b3d66d330be7ee31bcb7774690c5cb61c6d51
Nassir Isaf <njisaf@gmail.com>
2017-06-06 11:54:48 -0400

    cleaned up commented-out code

--- src/core/services/external-solutions.service.js
+++ src/core/services/external-solutions.service.js
@@ -30,20 +30,6 @@ class ExternalSolutionsService {
     return $('.title-large').css('background-image');
   }
 
-  // filterPSSDetails(htmlNode) {
-  //
-  //   const $ = cheerio.load(htmlNode);
-  //
-  //   return {
-  //     title: $('.article__main').find('article__title'),
-  //     bodyHeader: $('.article__body').find('h3'),
-  //     bodyText: $('.article__body').find('p'),
-  //     bodyList: $('.article__body').get('li'),
-  //     img: $('.widget_region').find('.card__image-inner img').attr('src'),
-  //     caption: $('.widget_region').find('card__title')
-  //   }
-  // }
-
 }
 
 export const externalSolutionsService = new ExternalSolutionsService();
