8f802f429114e8f7fa70b873470938f363d930d9
Nassir Isaf <njisaf@gmail.com>
2017-06-15 16:41:12 -0400

    make let a const again. katy's here in spirit.

--- src/core/stores/card-list.store.js
+++ src/core/stores/card-list.store.js
@@ -162,7 +162,7 @@ class CardListStore {
 
 	@computed get searchResultsCountLabel() {
 		if(!this.isLoading && this.searchHasBeenApplied) {
-			let count = this.filteredSearchResults.length;
+			const count = this.filteredSearchResults.length;
 			return `${count} Result${count === 1 ? '' : 's'}`
 		}
 	}
