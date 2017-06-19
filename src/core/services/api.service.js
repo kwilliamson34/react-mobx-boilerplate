19b74fcd8d61b1a1944a3a34fd56eb12255ca609
mlgantt <mgantt@sapient.com>
2017-06-19 14:16:38 -0400

    fix api url

--- src/core/services/api.service.js
+++ src/core/services/api.service.js
@@ -132,7 +132,7 @@ class ApiService {
     setMDMConfiguration(mdmConfig) {
       return axios({
         method: 'post',
-        url: `${base}/pse/mdmBla`,
+        url: `${base}/pse/mdm`,
         data: mdmConfig
       });
     }
