61bf4e3f96c0374f1deaade04718927b74552ac5
Nassir Isaf <njisaf@gmail.com>
2017-06-19 16:31:55 -0400

    form is working as intended. still missing success page.

--- src/core/stores/feedback.store.js
+++ src/core/stores/feedback.store.js
@@ -13,6 +13,8 @@ class FeedbackStore {
     if (this.formIsValid) {
       //awaiting service integration;
       console.log('Form submitted!  ', this.feedbackObject);
+      this.hasBeenSubmitted = true;
+      this.resetFeedbackForm();
     }
   }
 
@@ -20,14 +22,44 @@ class FeedbackStore {
     this.feedbackObject[input.id.replace('feedback-', '')] = input.value;
   }
 
+  @action toggleExitModal() {
+      this.showExitModal = !this.showExitModal;
+  }
+
+  @action discardFormChanges() {
+      this.showExitModal = false;
+      this.resetFeedbackForm();
+  }
+
+  @action resetFeedbackForm() {
+    this.showExitModal = false;
+    this.hasBeenSubmitted = false;
+    Object.keys(this.feedbackObject).forEach((v) => this.feedbackObject[v] = '');
+    Object.keys(this.hasErrors).forEach((v) => this.hasErrors[v] = false);
+  }
+
   @computed get formIsValid() {
     return Object.keys(this.hasErrors).every((v) => this.hasErrors[v] === false);
   }
 
   @computed get requiredFieldsEntered() {
-    return Object.keys(this.feedbackObject).every((v) => this.feedbackObject[v].length > 0);
+    let requiredFieldsEntered = true;
+    for (let key in this.feedbackObject) {
+      if (key !== 'email' && !this.feedbackObject[key].length) requiredFieldsEntered = false;
+    }
+    return requiredFieldsEntered;
   }
 
+  @computed get formHasEntries() {
+    let formHasEntries = false;
+    for (let key in this.feedbackObject) {
+      if (this.feedbackObject[key].length) formHasEntries = true;
+    }
+    return formHasEntries;
+  }
+
+  @observable showExitModal = false;
+  @observable hasBeenSubmitted = false;
   @observable feedbackObject = {
     title: '',
     details: '',
--- src/pages/feedback.page.jsx
+++ src/pages/feedback.page.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {inject, observer} from 'mobx-react';
+import {history} from '../core/services/history.service';
 
 @inject('store')
 @observer
@@ -15,6 +16,29 @@ export default class FeedbackPage extends React.Component {
     this.feedbackStore = this.props.store.feedbackStore;
   }
 
+  componentWillMount() {
+    this.feedbackStore.hasBeenSubmitted = false;
+  }
+
+  componentWillUnmount() {
+    if (this.feedbackStore.formHasEntries && !this.feedbackStore.hasBeenSubmitted) {
+      this.feedbackStore.showExitModal = true;
+      history.goBack();
+    }
+  }
+
+  toggleExitModal = (e) => {
+    e.preventDefault();
+    this.feedbackStore.toggleExitModal()
+  }
+
+  discardFormChanges = (e) => {
+    e.preventDefault();
+    console.log('triggered discard');
+    this.feedbackStore.discardFormChanges();
+    history.replace('/admin/');
+  }
+
   handleSubmit = (e) => {
     e.preventDefault();
     this.feedbackStore.submitForm(e.target);
@@ -23,10 +47,38 @@ export default class FeedbackPage extends React.Component {
   handleChange = (e) => {
     e.preventDefault();
     this.feedbackStore.changeValue(e.target);
+    // console.log('handleChange   ', this.feedbackStore.requiredFieldsEntered);
+  }
+
+  renderExitModal = () => {
+    return (
+      <div>
+        <div id="exitModal" className="modal fade in">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <button type="button" className="btn close-modal icon-close" onClick={this.toggleExitModal}>
+                <span className="sr-only">close window</span>
+              </button>
+              <div className="row no-gutters">
+                <div className="col-xs-12">
+                  <h1 className="as-h2">Unsaved changes</h1>
+                  <p>Your form changes will not be saved if you navigate away from this page.</p>
+                </div>
+                <div className="col-xs-12 text-center">
+                  <button className='fn-primary' onClick={this.toggleExitModal}>Stay on Page</button>
+                  <button className='fn-secondary' onClick={this.discardFormChanges}>Discard Changes</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="modal-backdrop fade in" ></div>
+      </div>
+    )
   }
 
   render() {
-
+    // console.log('render', this.feedbackStore.requiredFieldsEntered);
     return (
       <article id='help-center-page'>
         <section className='content-wrapper'>
@@ -43,14 +95,14 @@ export default class FeedbackPage extends React.Component {
                   {this.feedbackStore.hasErrors.title &&
                     <label className='control-label' htmlFor="feedback-title"><span>Please title your feedback</span></label>
                   }
-                  <input type='text' id='feedback-title' maxLength="250" className='form-control' onChange={this.handleChange}/>
+                  <input type='text' id='feedback-title' maxLength="250" className='form-control' value={this.feedbackStore.feedbackObject.title} onChange={this.handleChange}/>
                 </div>
                 <div className={this.feedbackStore.hasErrors.topic ? 'form-group has-error' : 'form-group'}>
                   <label className='control-label' htmlFor='feedback-topic'>Topic<span className='required-asterisks'> *</span></label>
                     {this.feedbackStore.hasErrors.topic &&
                       <label className='control-label' htmlFor="feedback-topic"><span>Please select a topic</span></label>
                     }
-                  <select id='feedback-topic' className='form-control' onChange={this.handleChange}>
+                  <select id='feedback-topic' className='form-control' value={this.feedbackStore.feedbackObject.topic} onChange={this.handleChange}>
                     <option value='' hidden>Select Feedback Topic</option>
                     <option value='System Performance'>System Performance</option>
                     <option value='App Management'>App Management</option>
@@ -66,7 +118,7 @@ export default class FeedbackPage extends React.Component {
                     {this.feedbackStore.hasErrors.details &&
                       <label className='control-label' htmlFor="feedback-details"><span>Please summarize your feedback</span></label>
                     }
-                  <input type='text' id='feedback-details' maxLength="10000" className='form-control' onChange={this.handleChange}/>
+                  <input type='text' id='feedback-details' maxLength="10000" className='form-control' value={this.feedbackStore.feedbackObject.details} onChange={this.handleChange}/>
                 </div>
                 <div>
                   <p>
@@ -75,7 +127,7 @@ export default class FeedbackPage extends React.Component {
                 </div>
                 <div className='form-group'>
                   <label className='control-label' htmlFor='feedback-email'>Email (Optional)</label>
-                  <input type='email' id='feedback-email' className='form-control' onChange={this.handleChange}/>
+                  <input type='email' id='feedback-email' className='form-control' value={this.feedbackStore.feedbackObject.email} onChange={this.handleChange}/>
                 </div>
                 <div className='form-group text-center'>
                   <button type='submit' id='feedback-submit-btn' className={this.feedbackStore.requiredFieldsEntered ? 'fn-primary' : 'fn-primary disabled'} aria-labelledby='feedback-form'>
@@ -87,6 +139,7 @@ export default class FeedbackPage extends React.Component {
           </div>
         </div>
         </section>
+        {this.feedbackStore.showExitModal && this.renderExitModal()}
       </article>
     )
   }
