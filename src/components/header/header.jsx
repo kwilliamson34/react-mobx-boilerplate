50690b8c364046254bda04c8419c95f01ee96d32
Paul Arce <parce@sapient.com>
2017-06-26 09:47:16 -0400

    Update of contact number to 800-574-7000 for both header and footer. * should resolve FPSE-723 and FPSE-753

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -240,9 +240,9 @@ export default class PSEHeader extends React.Component {
 											</NavLink>
 										</li>
 										<li>
-											<a href="tel:800-600-8000">
+											<a href="tel:800-574-7000">
 												<div className="multi-line-item">
-													AT&amp;T Customer Service: <phone><i className="icon-phone-number" aria-hidden="true"></i>800-600-8000</phone>
+													AT&amp;T Customer Service: <phone><i className="icon-phone-number" aria-hidden="true"></i>800-574-7000</phone>
 												</div>
 											</a>
 										</li>
@@ -307,8 +307,8 @@ export default class PSEHeader extends React.Component {
 											</NavLink>
 										</li>
 										<li role="presentation">
-											<a href="tel:800-600-8000" className="tel-cell">
-												AT&amp;T Customer Service: <br /><span><i className="icon-phone-number" aria-hidden="true"></i>800-600-8000</span>
+											<a href="tel:800-574-7000" className="tel-cell">
+												AT&amp;T Customer Service: <br /><span><i className="icon-phone-number" aria-hidden="true"></i>800-574-7000</span>
 											</a>
 										</li>
 									</ul>
