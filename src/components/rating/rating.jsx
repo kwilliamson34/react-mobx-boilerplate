f3e9ddc3dc309efdcfd7e7b3686a62aebdd229aa
Katy Williamson <kwilliamson@sapient.com>
2017-06-28 13:21:41 -0400

    FPSE-452 adding space before star icons on catalog page; cleaning up rating component

--- src/components/rating/rating.jsx
+++ src/components/rating/rating.jsx
@@ -1,62 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-import { observer } from 'mobx-react';
+import {observer} from 'mobx-react';
 
 @observer
 export class Rating extends React.Component {
 
-    constructor(props) {
-        super(props);
-    }
+  starTemplate(starImage, i) {
+    return (
+      <span key={i} aria-hidden="true" className={starImage}></span>
+    )
+  }
 
-    starTemplate(starImage, i) {
-        return  (
-            <span key={i} aria-hidden="true" className={starImage}></span>
-        )
-    }
+  convertRatingToStars(rating) {
+    const stars = [];
+    let remaining = rating;
 
-    convertRatingToStars(rating) {
-        const stars = [];
-        let remaining = rating;
-
-        for( let i = 5 ; i > 0 ; i-- ){
-            if (remaining >= 1){
-                stars.push(this.starTemplate('ratings-star icon-star', stars.length));
-                remaining--;
-            } else if (remaining > 0 ){
-                stars.push(this.starTemplate('ratings-star icon-star-half-o', stars.length));
-                remaining--;
-            } else {
-                stars.push(this.starTemplate('ratings-star icon-star-o', stars.length));
-            }
-        }
-        return stars;
+    for (let i = 5; i > 0; i--) {
+      if (remaining >= 1) {
+        stars.push(this.starTemplate('ratings-star icon-star', stars.length));
+        remaining--;
+      } else if (remaining > 0) {
+        stars.push(this.starTemplate('ratings-star icon-star-half-o', stars.length));
+        remaining--;
+      } else {
+        stars.push(this.starTemplate('ratings-star icon-star-o', stars.length));
+      }
     }
+    return stars;
+  }
 
-    render() {
-        let rating = this.props.rating ? this.props.rating : 0;
-        let stars = this.convertRatingToStars(rating)
-        return (
-            <div className="ratings-container">
-                <span className="ratings-number">
-                    <span className="sr-only">Rating of</span>
-                    {rating}
-                </span>
-                <span className="ratings-stars">
-                    {stars.map((star) => {
-                        return star;
-                    })}
-                </span>
-            </div>
-        );
-    }
+  render() {
+    let rating = this.props.rating ? this.props.rating : 0;
+    let stars = this.convertRatingToStars(rating)
+    return (
+      <div className="ratings-container">
+        <span className="ratings-number">
+          <span className="sr-only">Rating of</span>
+          {rating}
+        </span>&nbsp;
+        <span className="ratings-stars">
+          {stars}
+        </span>
+      </div>
+    );
+  }
 }
 
 Rating.propTypes = {
-    rating: PropTypes.number
+  rating: PropTypes.number
 };
 
 Rating.defaultProps = {
-    rating: 0
+  rating: 0
 }
