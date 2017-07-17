174361898c711e7811fc4840c9bb85e1ea734c0a
Nassir Isaf <njisaf@gmail.com>
2017-07-17 16:21:32 -0400

    snapshot test for rating component, 100% coverage. also some failed work on appReviews, not sure what's going wrong there.

--- src/components/app-reviews/__tests__/app-reviews.test.js
+++ src/components/app-reviews/__tests__/app-reviews.test.js
@@ -1,33 +1,34 @@
 jest.unmock('../app-reviews');
 jest.unmock('axios');
+jest.unmock('moment');
 jest.unmock('../../truncate/truncate');
 jest.unmock('../../rating/rating');
 
 import AppReviews from '../app-reviews';
-
-describe('Test test', () => {
-  it('should add two and two to equal four for god sake', () => {
-    expect(2 + 2).toBe(4);
-  });
-});
+import Truncate from '../../truncate/truncate';
+import {Rating} from '../../rating/rating';
 
 describe('<AppReviews />', () => {
-  let props = {
-    reviews: [{
-        commentTitle: 'Comment Title',
-        userFirstName: 'Firstname',
-        userLastName: 'Lastname',
-        reviewStar: 5,
-        reviewId: '12345',
-        reviewDate: '2017-05-16T15:59:55.025Z',
+
+  const props = {
+    reviews: [
+      {
+        appId: '12345',
         comment: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      }]
-    };
+        commentTitle: 'A Bad Review is Coming',
+        reviewDate: '2017-05-16T15:59:55.025Z',
+        reviewId: '12345',
+        reviewStar: 1,
+        userFirstName: 'Cletus Slackjaw',
+        userLastName: 'of the House Yokel'
+      }
+    ]
+  }
 
   describe('renders', () => {
     test('matches previous snapshot', () => {
         const component = renderer.create(
-          <AppReviews {...props} />
+          <AppReviews {...props}></AppReviews>
         );
 
         let tree = component.toJSON();
