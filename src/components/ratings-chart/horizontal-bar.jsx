fa368d060f245fd531713132b3f0740b2b66512d
Katy Williamson <kwilliamson@sapient.com>
2017-07-18 14:22:53 -0400

    updating ratings chart to avoid local state, use static props; resolving console error

--- src/components/ratings-chart/horizontal-bar.jsx
+++ src/components/ratings-chart/horizontal-bar.jsx
@@ -1,45 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-
 export default class HorizontalBar extends React.Component {
-	constructor(props) {
-		super(props);
-	}
+  static propTypes = {
+    data: PropTypes.object
+  }
 
-	render() {
-		const max = Math.max.apply(Math, this.props.data);
+  static defaultProps = {
+    data: {}
+  }
 
-
-		return (
-			<div className={'horizontal-bar-chart'}>
-        {
-          this.props.data.map(function (item, itemIndex) {
-						let size = 1;
-						if (item > 0) size = item/max * 100;
-            let style = {width:size + '%'}
-            return (
-              <div className="bar-wrapper" key={itemIndex+'bar'}>
-                <label className="ratings-container">
-									<i aria-hidden="true" className="ratings-star icon-star"></i>
-                  <span className="ratings-number" aria-hidden="true">{(5-itemIndex)}</span>
-                </label>
-                <div className="bar" style={style}>
-                  <div className="bar-value">{item} <span className="sr-only">{(5-itemIndex)} star reviews submitted</span></div>
+  render() {
+    const max = Math.max.apply(Math, this.props.data);
+    return (
+      <div className={'horizontal-bar-chart'}>
+        {this.props.data.map((item, itemIndex) => {
+          let size = 1;
+          if (item > 0)
+            size = item / max * 100;
+          let style = {
+            width: size + '%'
+          }
+          return (
+            <div className="bar-wrapper" key={itemIndex + 'bar'}>
+              <label className="ratings-container">
+                <i aria-hidden="true" className="ratings-star icon-star"></i>
+                <span className="ratings-number" aria-hidden="true">{(5 - itemIndex)}</span>
+              </label>
+              <div className="bar" style={style}>
+                <div className="bar-value">
+                  {item}
+                  <span className="sr-only">of reviews gave {(5 - itemIndex)} stars</span>
                 </div>
               </div>
-            );
-          })
-        }
-        </div>
-		);
-	}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
-
-HorizontalBar.propTypes = {
-	data: PropTypes.array // value the chart should show
-};
-
-HorizontalBar.defaultProps = {
-	data: [0]
-};
