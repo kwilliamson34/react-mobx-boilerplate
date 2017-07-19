0cbf58926776f5bfd5da0af54539ef971794c4f9
Katy Williamson <kwilliamson@sapient.com>
2017-07-19 17:33:51 -0400

    fixing and extending card list store tests

--- src/core/stores/__tests__/card-list.store.test.js
+++ src/core/stores/__tests__/card-list.store.test.js
@@ -9,31 +9,69 @@ import { history } from '../../services/history.service';
 import { cardListStore } from '../card-list.store';
 
 const store = cardListStore;
-// set your store here, you're testing actions against the store, not the store itself
+
+let arrayOfSixApps = [
+  {title: "one", category: ["cat1"], user_segment: ["FIRE & RESCUE"], operatingSystem: "ANDROID", isRecommended: true},
+  {title: "two", category: ["cat1"], user_segment: ["FIRE & RESCUE","LAW ENFORCEMENT","HAZMAT"], operatingSystem: "IOS", isRecommended: false},
+  {title: "three", category: ["cat1"], user_segment: ["LAW ENFORCEMENT"], operatingSystem: "IOS", isRecommended: false},
+  {title: "four", category: ["cat1"], user_segment: ["EMERGENCY MANAGEMENT"], operatingSystem: "IOS", isRecommended: false},
+  {title: "five", category: ["cat1","cat2"], user_segment: ["HAZMAT"], operatingSystem: "IOS", isRecommended: true},
+  {title: "six", category: ["cat2"], user_segment: ["HAZMAT"], operatingSystem: "ANDROID", isRecommended: false}
+];
 
 describe("FilterStore", () => {
 
   beforeAll(() => {
-    store.searchResults = [{title: "one", custom_metadata: {user_segment: [1]}, isRecommended: true},
-      {title: "two", custom_metadata: {user_segment: ["FIRE & RESCUE"]}, isRecommended: false},
-      {title: "three", custom_metadata: {user_segment: ["LAW ENFORCEMENT"]}, isRecommended: false},
-      {title: "four", custom_metadata: {user_segment: ["EMERGENCY MANAGEMENT"]}, isRecommended: false},
-      {title: "five", custom_metadata: {user_segment: [1]}, isRecommended: true},
-      {title: "six", custom_metadata: {user_segment: ["HAZMAT DISPATCH"]}, isRecommended: false}];
+    store.searchResults = arrayOfSixApps;
   });
 
-  test("is not filtered initially", () => {
+  test("is not filtered or searched initially", () => {
     expect(store.filteredSearchResults.length).toBe(6)
-    expect(store.isFiltered).toBe(false);
+    expect(store.searchIsApplied).toBe(false);
+    expect(store.platformFilter).toBe('');
+    expect(store.categoryFilter).toBe('');
+    expect(store.segmentFilter).toBe('');
   });
 
   test("filters list after applying filter", () => {
     store.changeSegmentFilter("FIRE & RESCUE");
+    expect(store.segmentFilter).toBe("FIRE & RESCUE");
+    expect(store.filteredSearchResults.length).toBe(2);
+
+    store.changeSegmentFilter("EMERGENCY MANAGEMENT");
+    expect(store.segmentFilter).toBe("EMERGENCY MANAGEMENT");
     expect(store.filteredSearchResults.length).toBe(1);
+
+    store.changeSegmentFilter("");
+    store.changeCategoryFilter("cat2");
+    expect(store.categoryFilter).toBe("cat2");
+    expect(store.filteredSearchResults.length).toBe(2);
+
+    store.changeCategoryFilter("");
+    store.changePlatformFilter("IOS");
+    expect(store.platformFilter).toBe("IOS");
+    expect(store.filteredSearchResults.length).toBe(4);
   });
 
-  test("is filtered after applying filter", () => {
-    expect(store.isFiltered).toBe(true);
+  test("can filter by level of recommendation", () => {
+      expect(store.recommendedCards.length).toBe(2);
+  });
+
+  test("updates appropriately when user resets their filters", () => {
+    store.searchResults = arrayOfSixApps;
+    store.changeSegmentFilter("");
+    store.changeCategoryFilter("");
+    store.changePlatformFilter("");
+    expect(store.filteredSearchResults.length).toBe(6);
+
+    store.changePlatformFilter("ANDROID");
+    expect(store.platformFilter).toBe("ANDROID");
+    expect(store.filteredSearchResults.length).toBe(2);
+
+    store.resetFilters();
+    expect(store.platformFilter).toBe("");
+    expect(store.filteredSearchResults.length).toBe(6);
+    expect(store.filterIsApplied).toBe(false);
   });
 
 });
@@ -42,20 +80,7 @@ describe("CardListStore", () => {
 
     // will run once before everything
     beforeAll(() => {
-      store.searchResults = [{title: "one", custom_metadata: {user_segment: [1]}, isRecommended: true},
-        {title: "two", custom_metadata: {user_segment: ["FIRE & RESCUE"]}, isRecommended: false},
-        {title: "three", custom_metadata: {user_segment: ["LAW ENFORCEMENT"]}, isRecommended: false},
-        {title: "four", custom_metadata: {user_segment: ["EMERGENCY MANAGEMENT"]}, isRecommended: false},
-        {title: "five", custom_metadata: {user_segment: [1]}, isRecommended: true},
-        {title: "six", custom_metadata: {user_segment: ["HAZMAT DISPATCH"]}, isRecommended: false}];
-
-        store.segments = [
-          {display: '', name: 'FIRE & RESCUE'},
-          {display: '', name: 'LAW ENFORCEMENT'},
-          {display: '', name: 'HAZMAT DISPATCH'},
-          {display: '', name: 'EMERGENCY MANAGEMENT'},
-          {display: '', name: 'recommended'},
-        ]
+      store.searchResults = arrayOfSixApps;
     });
 
     // will run once after everything
@@ -67,47 +92,63 @@ describe("CardListStore", () => {
         expect(store.searchResults.length).toBe(6);
     });
 
-    test("Homepage Cards has the right segments", () => {
-      expect(Object.keys(store.homepageCards).length).toBe(5)
-      expect(store.homepageCards['FIRE & RESCUE'][0].title).toBe("two");
-      expect(store.homepageCards['LAW ENFORCEMENT'][0].title).toBe("three");
-      expect(store.homepageCards['EMERGENCY MANAGEMENT'][0].title).toBe("four");
-      expect(store.homepageCards['HAZMAT DISPATCH'][0].title).toBe("six");
-      expect(store.homepageCards['recommended'].length).toBe(2);
-    })
+    test("updates appropriately when user clears all search and filter inputs", () => {
+      store.originalCardList = arrayOfSixApps;
+      store.searchResults = [];
+      store.changePlatformFilter("ANDROID");
+      store.changeSegmentFilter("HAZMAT");
+      store.handleSearchInput("hello");
+
+      store.restoreOriginalList();
+      expect(store.platformFilter).toBe("");
+      expect(store.segmentFilter).toBe("");
+      expect(store.categoryFilter).toBe("");
+      expect(store.searchQuery).toBe("");
+      expect(store.searchIsApplied).toBe(false);
+      expect(store.searchResults).toBe(arrayOfSixApps);
+    });
 });
 
 describe("SearchStore", () => {
 
   beforeAll(() => {
-    store.clearFilters();
-    store.searchResults = [{title: "one", user_segment: [1], isRecommended: true}]
+    store.resetFilters();
+    store.searchResults = [{title: "one", category:"cat1", user_segment: ["FIRE & RESCUE"], isRecommended: true}]
     history.push = jest.fn();
   });
 
   test("accepts input", () => {
     let testText = "gibberish";
-    store.handleInput(testText);
+    store.handleSearchInput(testText);
     expect(store.searchQuery).toBe(testText);
   });
 
-  test("gets search results successfully", () => {
-    expect(store.searchResults).toBeTruthy();
+  test("generates correct result count label", () => {
+    store.isLoading = false;
+    expect(store.resultsCountLabel).toBe(undefined);
+
+    store.searchIsApplied = true;
+
+    store.resetFilters();
+    store.searchResults = [];
+    expect(store.resultsCountLabel).toBe("");
+
+    store.searchResults = [{title: "one", category:"cat1", user_segment: ["FIRE & RESCUE"], isRecommended: true}]
+    expect(store.resultsCountLabel).toBe("1 Result");
+
+    store.searchResults = arrayOfSixApps;
+    expect(store.resultsCountLabel).toBe("6 Results");
   });
 
-  test("calculates populated result set size", () => {
-    expect(store.numSearchResults).toBe(1);
-  });
+  test("updates appropriately when user clears their search", () => {
+    store.originalCardList = arrayOfSixApps;
+    store.searchIsApplied = true;
+    store.searchQuery = "rescue";
+    store.searchResults = [{title: "one", category:"cat1", user_segment: ["FIRE & RESCUE"], isRecommended: true}];
 
-  test("displays search component", () => {
-    store.searchIconClick();
-    expect(store.searchIsVisible).toBe(true);
+    store.clearSearchQuery();
+    expect(store.searchQuery).toBe("");
+    expect(store.searchResults).toBe(arrayOfSixApps);
+    expect(store.searchIsApplied).toBe(false);
   });
-
-  test("hides search component", () => {
-    store.handleClose();
-    expect(store.searchIsVisible).toBe(false);
-    expect(store.searchQuery).toBeFalsy();
-  });
-
 });
