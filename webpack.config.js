f45a4b28efc17cf4077c2b4097b56d7dde4bfe51
Paul Arce <parce@sapient.com>
2017-07-26 16:21:00 -0400

    Adding stage environment for testing deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-//env variable passed in from webpack --env=(dev|prod) build call
+//env variable passed in from webpack --env=(dev|stage|prod) build call
 module.exports = function(env) {
   let _env = env || 'prod';
   return require(`./webpack.config.${_env}.js`)
