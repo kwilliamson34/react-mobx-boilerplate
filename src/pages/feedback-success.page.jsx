b91c0abc4321a9fed4a26ff249f8938d91c911e3
Katy Williamson <kwilliamson@sapient.com>
2017-08-22 14:45:13 -0400

    use error page styling for success pages

--- src/pages/feedback-success.page.jsx
+++ src/pages/feedback-success.page.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
-import {inject, observer} from 'mobx-react';
-import {history} from '../core/services/history.service';
+import {Link} from 'react-router-dom';
 
-@inject('store')
-@observer
-export default class FeedbackPage extends React.Component {
-  goHome = (e) => {
-    e.preventDefault();
-    history.replace('/');
-  }
-
+export default class FeedbackSuccessPage extends React.Component {
   render() {
     return (
       <section className="success-page">
-        <div className="success-content">
-          <h1>Thanks for your feedback!</h1>
-          <p>We appreciate you taking the time to provide your thoughts about this site. Your comments will help us to improve our tools going forward.</p>
-          <button className="fn-primary" onClick={this.goHome}>Return Home</button>
+        <div className="container">
+          <div className="col-xs-12 col-md-offset-1 col-md-10 col-lg-offset-1 col-lg-10">
+            <h1>Thanks for your feedback!</h1>
+            <p>We appreciate you taking the time to provide your thoughts about this site. Your comments will help us to improve our tools going forward.</p>
+            <Link to="/" className="fn-primary">Return to Home Page</Link>
+          </div>
         </div>
       </section>
     )
