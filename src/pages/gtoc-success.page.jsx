94718300d67003e7bbc6d6f8f2421f0392f4f53d
Nassir Isaf <nisaf@sapient.com>
2017-09-07 15:39:27 -0400

    final design tweaks.

--- src/pages/gtoc-success.page.jsx
+++ src/pages/gtoc-success.page.jsx
@@ -10,7 +10,11 @@ export default class SubscribeToGTOCSuccess extends React.Component {
           <div className="col-xs-12 col-md-offset-1 col-md-10 col-lg-offset-1 col-lg-10">
             <h1>We've received your request.</h1>
             <p>
-              Processing your subscription can take up to 14 days, after which you will start receiving alert notifications. If you have any questions, please contact<br className="visible-lg-block"/> FirstNet Customer Service at {config.attCustomerSupportPhone}.
+              Processing your subscription can take up to 14 days, after which you will start receiving alert notifications. If you have any questions, please contact<br className="visible-lg-block"/> FirstNet Customer Service at&nbsp;
+              <a href={'tel:' + config.attCustomerSupportPhone}>
+								<span className="sr-only">FirstNet Customer Service Phone&nbsp;</span>
+								{config.attCustomerSupportPhone}
+							</a>.
             </p>
             <Link to="/network-status" className="fn-primary">Network Status</Link>
           </div>
