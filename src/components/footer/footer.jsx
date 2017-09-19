ec147555f5ed4dad2eec0f797a49f351c2ab0435
Katy Williamson <kwilliamson@sapient.com>
2017-09-19 17:48:55 -0400

    adding training link to desktop header, mobile header, and footer with existing external link icon treatments

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -12,11 +12,11 @@ export default class Footer extends React.Component {
 
 	static propTypes = {
 		store: PropTypes.object,
-		showInternalLinks: PropTypes.bool
+		showPrivateLinks: PropTypes.bool
 	}
 
 	static defaultProps = {
-    showInternalLinks: true
+    showPrivateLinks: true
   }
 
 	constructor(props) {
@@ -35,7 +35,7 @@ export default class Footer extends React.Component {
 	renderSitemapColumn() {
 		let sitemapCollapseBreakpoint = 992;
 		let allowFocusOnSitemapHeader = this.headerStore.viewportWidth < sitemapCollapseBreakpoint;
-		if(this.props.showInternalLinks){
+		if(this.props.showPrivateLinks){
 			return(
 				<div className="col-xs-12 col-md-3">
 					<nav aria-describedby="sitemap">
@@ -75,7 +75,7 @@ export default class Footer extends React.Component {
 
 	renderFirstNetColumn() {
 		return (
-			<div className={`col-xs-12 ${this.props.showInternalLinks ? 'col-md-3' : 'col-md-4'}`}>
+			<div className={`col-xs-12 ${this.props.showPrivateLinks ? 'col-md-3' : 'col-md-4'}`}>
 				<nav aria-describedby="firstnet-sites">
 					<h2 id="firstnet-sites">Firstnet Sites</h2>
 					<ul >
@@ -99,7 +99,7 @@ export default class Footer extends React.Component {
 
 	renderSocialLinkColumn() {
 		return (
-			<div className={`col-xs-12 ${this.props.showInternalLinks ? 'col-md-3' : 'col-md-4'}`}>
+			<div className={`col-xs-12 ${this.props.showPrivateLinks ? 'col-md-3' : 'col-md-4'}`}>
 				<nav className="social-nav" aria-describedby="social-links">
 					<h2 id="social-links">Follow Us</h2>
 					<ul className="social-links-list">
@@ -141,22 +141,29 @@ export default class Footer extends React.Component {
 
 	renderHelpColumn() {
 		return (
-			<div className={`col-xs-12 ${this.props.showInternalLinks ? 'col-md-3' : 'col-md-4'}`}>
+			<div className={`col-xs-12 ${this.props.showPrivateLinks ? 'col-md-3' : 'col-md-4'}`}>
 				<nav aria-describedby="helpLinks">
 					<h2 id="helpLinks" className="help-hdr">Help</h2>
 					<ul className="help-link-block">
-						{this.props.showInternalLinks &&
+						{this.props.showPrivateLinks &&
 							<li role="presentation">
 								<Link to="/faq">FAQ</Link>
 							</li>
 						}
-						{this.props.showInternalLinks &&
+						{this.props.showPrivateLinks &&
+							<li role="presentation">
+								<NewTabLink to={this.externalLinkStore.firstnetTraining}>
+									Training
+								</NewTabLink>
+							</li>
+						}
+						{this.props.showPrivateLinks &&
 							<li role="presentation">
 								<Link to="/feedback">Give Us Feedback</Link>
 							</li>
 						}
 						<li role="presentation">
-							<span aria-hidden>FirstNet Customer Svc:</span><br className={`visible-xs-inline-block visible-md-inline-block ${this.props.showInternalLinks ? '' : 'visible-lg-inline-block'}`} />
+							<span aria-hidden>FirstNet Customer Svc:</span><br className={`visible-xs-inline-block visible-md-inline-block ${this.props.showPrivateLinks ? '' : 'visible-lg-inline-block'}`} />
 							<a href={'tel:' + config.attCustomerSupportPhone}>
 								<i className="icon-phone-number footer-support-phone" aria-hidden='true'></i>
 								<span className="sr-only">FirstNet Customer Service Phone&nbsp;</span>
@@ -204,12 +211,11 @@ export default class Footer extends React.Component {
 					<div className="container">
 						<div className="row">
 							<div className="col-xs-12 logoRow">
-								{(this.props.showInternalLinks)?
-									<Link to="/" className="logo-home-link-footer">
-										<img src="/images/logo-FirstNet-local-control.svg" alt="FirstNet Local Control Home" />
-									</Link>
-									:
-									<img src="/images/logo-FirstNet-local-control.svg" alt="" aria-hidden="true"/>
+								{this.props.showPrivateLinks
+									? <Link to="/" className="logo-home-link-footer">
+											<img src="/images/logo-FirstNet-local-control.svg" alt="FirstNet Local Control Home" />
+										</Link>
+									: <img src="/images/logo-FirstNet-local-control.svg" alt="" aria-hidden="true"/>
 								}
 							</div>
 						</div>
