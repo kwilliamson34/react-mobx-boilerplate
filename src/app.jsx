93585494c6e1c7e6ba4582cef5416523c45793bc
Paul Arce <parce@sapient.com>
2017-09-19 21:49:53 -0400

    spelling walkthru correctly so that Katy doesnt shank me

--- src/app.jsx
+++ src/app.jsx
@@ -122,7 +122,7 @@ export default class App extends React.Component {
   getMainLayoutComponent = () => {
     return (
       <div>
-        {config.showOnboardingWalkthru && pseMasterStore.joyrideStore.isTourIncomplete() &&
+        {config.showOnboardingWalkthrough && pseMasterStore.joyrideStore.isTourIncomplete() &&
 					<JoyrideBase location={location.pathname} joyrideStore={pseMasterStore.joyrideStore} />
 				}
         <ScrollToTop>
