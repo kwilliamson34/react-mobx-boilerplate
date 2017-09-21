e33fe032902a068d5b31d8eaa361eebebb0bceae
Paul Arce <parce@sapient.com>
2017-09-21 14:52:16 -0400

    new detection

--- src/core/services/utils.service.js
+++ src/core/services/utils.service.js
@@ -168,13 +168,35 @@ class UtilsService {
   }
 
   getIsInternetExplorer() {
-    if (navigator.appName == 'Microsoft Internet Explorer'){
-      return true; // IE
-    } else if(navigator.appName == "Netscape"){
-      return navigator.appVersion.indexOf('Edge') > -1; // EDGE
-    }
-    return false;
+    var ua = window.navigator.userAgent;
+
+  var msie = ua.indexOf('MSIE ');
+  if (msie > 0) {
+    // IE 10 or older => return version number
+    return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
   }
+
+  var trident = ua.indexOf('Trident/');
+  if (trident > 0) {
+    // IE 11 => return version number
+    var rv = ua.indexOf('rv:');
+    return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+  }
+
+  var edge = ua.indexOf('Edge/');
+  if (edge > 0) {
+    // Edge (IE 12+) => return version number
+    return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+  }
+
+  // other browser
+  return false;
+}
+
+
+
+
+
 }
 
 export const utilsService = new UtilsService();
