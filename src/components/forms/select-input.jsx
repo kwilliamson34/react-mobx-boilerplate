c58d9d678b448a5179e0342919b61de137e2cb63
Katy Williamson <kwilliamson@sapient.com>
2017-09-27 14:31:54 -0400

    adding validation and blur to form components; moving feedback form

--- src/components/forms/select-input.jsx
+++ src/components/forms/select-input.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {observer} from 'mobx-react';
-import {observable} from 'mobx';
+import {observable, computed} from 'mobx';
 
 import FormLabel from './form-label';
 
@@ -16,6 +16,7 @@ export default class SelectInput extends React.Component {
     optionsList: PropTypes.array,
     placeholder: PropTypes.string,
     disabled: PropTypes.bool,
+    validate: PropTypes.func,
     errorMessage: PropTypes.string
   }
 
@@ -28,16 +29,30 @@ export default class SelectInput extends React.Component {
   }
 
   @observable hasError = false;
+  @computed get valueInStore() {
+    return this.props.dataObject[this.props.id];
+  }
 
   handleOnChange = (e) => {
     this.props.dataObject[this.props.id] = e.target.value;
+  }
+
+  handleOnBlur = () => {
+    console.log('select on blur');
+    //check emptiness last, as it should overwrite a truthy validation response
+    if(this.props.validate) {
+      this.hasError = this.props.validate();
+    }
+    if(this.props.required && !this.valueInStore) {
+      this.hasError = true;
+    }
   }
 
   render() {
     const value = this.props.dataObject[this.props.id];
 
     return (
-      <div className="form-group">
+      <div className={`form-group ${this.hasError ? 'has-error' : ''}`}>
         <FormLabel
           id={this.props.id}
           hasError={this.hasError}
@@ -48,6 +63,7 @@ export default class SelectInput extends React.Component {
           className="form-control form-control-lg"
           id={this.props.id}
           onChange={this.handleOnChange}
+          onBlur={this.handleOnBlur}
           value={value}
           disabled={this.props.disabled}>
           {this.props.placeholder && <option value="">{this.props.placeholder}</option>}
