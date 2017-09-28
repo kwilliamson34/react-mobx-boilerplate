589fe94b45402570e518e6c71990e33c061515ba
Katy Williamson <kwilliamson@sapient.com>
2017-09-28 14:50:52 -0400

    solutions card common component

--- src/pages/solutions-category.template.jsx
+++ src/pages/solutions-category.template.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { observer, inject } from 'mobx-react';
-import { Link } from 'react-router-dom';
+import { utilsService } from '../core/services/utils.service';
 
-import {utilsService} from '../core/services/utils.service';
 import BreadcrumbNav from '../components/breadcrumb-nav/breadcrumb-nav';
+import SolutionCard from '../components/solutions/solution-card';
 
 @inject('store')
 @observer
@@ -30,52 +30,16 @@ export default class SolutionsCategoryTemplate extends React.Component {
     }
   }
 
-  getGradientStyles(imageUrl) {
-    const shadowLength = '39%';
-    const shadowOpacity = '0.85';
-    let backgroundImage = '';
-    if(utilsService.getIsInternetExplorer()) {
-      backgroundImage = `-ms-linear-gradient(bottom, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0) ${shadowLength}, rgba(0, 0, 0, ${shadowOpacity}) 100%), url('${imageUrl}')`
-    } else {
-      backgroundImage = `linear-gradient(to top, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0) ${shadowLength}, rgba(0, 0, 0, ${shadowOpacity}) 100%), url('${imageUrl}')`
-    }
-    return {backgroundImage}
-  }
-
-  getNormalStyles(imageUrl) {
-    return {
-      background: `url('${imageUrl}') no-repeat`,
-      backgroundPosition: 'center',
-      backgroundSize: 'cover'
-    }
-  }
-
   renderCards = (solutionsArray) => {
     return solutionsArray.map((solution) => {
-      const solutionUrl = `${this.props.match.url}/${utilsService.getDevicesAndSolutionsUrl(solution.promo_title)}`;
-      const hasValidRelatedApp = this.externalLinkStore.hasValidRelatedApp(solution);
-      return (
-        <div key={solution.promo_title} className="col-xs-12 col-sm-6 col-md-6 col-lg-4 solutions-card">
-          <div className="card-wrapper has-shadow">
-            <Link to={solutionUrl}>
-              <div className="card-img-wrapper">
-                {hasValidRelatedApp
-                  ? <p className="is-linked">App Available</p>
-                  : ''}
-                {hasValidRelatedApp
-                  ? <div className="img" style={this.getGradientStyles(solution.promo_image_url)} alt={solution.promo_title}></div>
-                  : <div className="img" style={this.getNormalStyles(solution.promo_image_url)} alt={solution.promo_title}></div>}
-              </div>
-              <div className="card-contents-wrapper">
-                <h2 className="card-title as-h3" dangerouslySetInnerHTML={{__html: solution.promo_title}}></h2>
-                <div className="card-desc" dangerouslySetInnerHTML={{__html: solution.promo_description}}></div>
-              </div>
-              <div className="learn-more">Learn More<i className="icon-arrowRight" aria-hidden="true" /></div>
-            </Link>
-          </div>
-        </div>
-      )
-    })
+      return <SolutionCard
+                key={solution.promo_title}
+                linkTo={`${this.props.match.url}/${utilsService.getDevicesAndSolutionsUrl(solution.promo_title)}`}
+                title={solution.promo_title}
+                imageUrl={solution.promo_image_url}
+                description={solution.promo_description}
+                hasValidRelatedApp={this.externalLinkStore.hasValidRelatedApp(solution)}/>
+    });
   }
 
   render() {
