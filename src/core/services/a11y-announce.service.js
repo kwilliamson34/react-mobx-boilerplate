e4dce7dc585ac61947247dc085ec4a3dcfed0373
Paul Arce <parce@sapient.com>
2017-09-28 16:14:26 -0400

    fixing some glaring typos.   **blames jazz....

--- src/core/services/a11y-announce.service.js
+++ src/core/services/a11y-announce.service.js
@@ -1,8 +1,8 @@
 import $ from 'jquery';
 /**
  * Adds text to a visually hidden elem with a role="alert" that gets read by a screen reader.
- * Clears out the existing text bore adding, since screen readers will only read CHANGES in the element. The element is index.html and isn't within the React root element.
- * Use this when adding a hdden alert within a React component isn't feasiable, for example if the component will unmount before or soon after the message is meant to be added.
+ * Clears out the existing text before adding, since screen readers will only read CHANGES in the element. The element is index.html and isn't within the React root element.
+ * Use this when adding a hidden alert within a React component isn't feasible, for example if the component will unmount before or soon after the message is meant to be added.
  * @param message
  * @messageType - importance level: alert, status
  * @param resetBefore
