081d35b916208d57eef9fcb0db1d1ec8f547a709
Paul Arce <parce@sapient.com>
2017-10-02 09:25:19 -0400

    nbsp; was breaking page title announcer

--- src/pages/session-timeout.page.jsx
+++ src/pages/session-timeout.page.jsx
@@ -26,7 +26,7 @@ export default class SessionTimeoutPage extends React.Component {
               <div className="fnnav__header">
                 <div className="fnnav__brand">
                   <div className="logo-container">
-                    <img src="/images/logo-FirstNet-local-control.svg" alt="" aria-hidden="true"/>
+                    <img src="/images/logo-FirstNet-local-control.svg" alt="" aria-hidden="true" />
                   </div>
                 </div>
               </div>
@@ -36,7 +36,7 @@ export default class SessionTimeoutPage extends React.Component {
         <section className="session-timeout">
           <div className="container">
             <div className="col-xs-12 col-md-offset-1 col-md-10 col-lg-offset-1 col-lg-10 ">
-              <PageTitle childrenContainHTML={true} className="as-h2">Your session has timed out.&nbsp;<br className="hidden-xs"/>Please log in again.</PageTitle>
+              <PageTitle childrenContainHTML={true} className="as-h2">Your session has timed out. <br className="hidden-xs" />Please log in again.</PageTitle>
               <div className="text-center">
                 <a href={config.haloLogin} className="fn-primary">Go to Login</a>
               </div>
