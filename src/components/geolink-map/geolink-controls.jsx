de8be4a2282a45608e12d950b9f6b0f09677e94d
Katy Williamson <kwilliamson@sapient.com>
2017-10-09 16:04:06 -0400

    geolink controls layout adjustments

--- src/components/geolink-map/geolink-controls.jsx
+++ src/components/geolink-map/geolink-controls.jsx
@@ -68,58 +68,54 @@ export default class GeolinkControls extends React.Component {
       <section className="geolink-controls">
         <div className="container">
           <div className="row is-flex">
-            <div className="col-xs-12 col-sm-8 col-md-4 map-search">
-              <h2 className="as-h5">Search</h2>
-              <form className="search-form form-group" onSubmit={this.handleSubmit}>
-                <div className="search-input input-group">
-                  <label htmlFor="search-field" className="control-label">Location</label>
-                  <div className="search-bar">
-                    <input id="search-field" type="search" ref="searchInput" disabled={this.props.disabled} className="form-control" onChange={this.handleSearchInput} onKeyPress={this.handleSearchKeyPress}/>
-                    <button className="btn search-btn" type="button" onClick={this.handleSearchSubmit} disabled={this.props.disabled}>
-                      <span className="sr-only">Search for locations</span>
-                      <span aria-hidden="true" className="icon-search"/>
-                    </button>
-                  </div>
-                </div>
-              </form>
+            <div className="col-xs-12 col-sm-7 col-md-4 map-search">
+              {this.renderSearchArea()}
             </div>
-            <div className="col-xs-12 col-sm-4 col-md-4 map-layers">
-              <h2 className="as-h5">Layers</h2>
+            <div className="col-xs-12 col-sm-5 col-md-4 map-layers">
+              <h2>Layers</h2>
               <form className="form-group">
                 <fieldset className="coverage-layers">
                   <legend className="sr-only">Coverage layers</legend>
-                  <div className="col-xs-6 col-sm-12 no-gutters">
+                  <div className="col-xs-6 no-gutters">
                     <Checkbox label="Network" handleOnChange={this.toggleNetwork} checked={this.geoStore.showNetworkLayer} disabled={this.props.disabled}/>
                     <Checkbox label="Weather" handleOnChange={this.toggleWeather} checked={this.geoStore.showWeatherLayer} disabled={this.props.disabled}/>
                   </div>
-                  <div className="col-xs-6 col-sm-12 no-gutters">
+                  <div className="col-xs-6 no-gutters">
                     <Checkbox label="Traffic" handleOnChange={this.toggleTraffic} checked={this.geoStore.showTrafficLayer} disabled={this.props.disabled}/>
                     <Checkbox label="Alerts" handleOnChange={this.toggleAlerts} checked={this.geoStore.showAlertLayer} disabled={this.props.disabled || !this.geoStore.authIsComplete}/>
                   </div>
                 </fieldset>
               </form>
             </div>
+
+            {/* desktop layout */}
             <div className="col-md-4 hidden-xs hidden-sm map-network-legend">
               {this.renderNetworkLegend()}
-              {this.renderNetworkSubscriptionLink()}
               {this.renderContactInfo()}
             </div>
           </div>
 
-          <div className="row">
-            <div className="legend-divider col-xs-12">
+          {/* tablet and phone layout */}
+          <div className="row is-flex visible-xs visible-sm">
+            <div className="col-xs-12 no-gutters">
               <hr />
-              <div className="visible-xs-inline">
-                {this.renderNetworkSubscriptionLink()}
-                {this.renderContactInfo()}
-                <hr />
-              </div>
+            </div>
+            <div className="col-xs-12 col-sm-6">
+              {this.renderNetworkSubscriptionLink()}
+            </div>
+            <div className="col-xs-12 col-sm-6">
+              {this.renderContactInfo()}
+            </div>
+            <div className="col-xs-12 no-gutters">
+              <hr />
             </div>
           </div>
+
           <div className="row is-flex">
             <div className="col-xs-12 visible-xs-inline visible-sm-inline">
               {this.renderNetworkLegend()}
             </div>
+            <hr className="col-xs-12 hidden-xs hidden-sm" />
             <div className="col-xs-12 col-md-6">
               {this.renderWeatherLegend()}
             </div>
@@ -132,11 +128,34 @@ export default class GeolinkControls extends React.Component {
     );
   }
 
+  renderSearchArea = () => {
+    return (
+      <div>
+        <span className="top-right-link">
+          <Link to="/manage-favorites">Manage Favorites</Link>
+        </span>
+        <h2>Search</h2>
+        <form className="search-form form-group" onSubmit={this.handleSubmit}>
+          <div className="search-input input-group">
+            <label htmlFor="search-field" className="control-label">Location</label>
+            <div className="search-bar">
+              <input id="search-field" type="search" ref="searchInput" disabled={this.props.disabled} className="form-control" onChange={this.handleSearchInput} onKeyPress={this.handleSearchKeyPress}/>
+              <button className="btn search-btn" type="button" onClick={this.handleSearchSubmit} disabled={this.props.disabled}>
+                <span className="sr-only">Search for locations</span>
+                <span aria-hidden="true" className="icon-search"/>
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    )
+  }
+
   renderNetworkSubscriptionLink = () => {
     return (
       <div className="network-subscription-link">
         <Link to="/subscribe-to-alerts">
-          Subscribe to Network Alerts
+          Subscribe to <span className="visible-xs-inline visible-sm-inline">Network </span>Alerts
         </Link>
       </div>
     )
@@ -144,30 +163,27 @@ export default class GeolinkControls extends React.Component {
 
   renderContactInfo = () => {
     return (
-      <div className="network-contact-info">
-        <p><span aria-hidden="true">Report Network Issue:</span>
-          <br className="visible-md-inline"/>
-          <a href={'tel:' + this.geoStore.networkIssueNumber}>
-            <span>
-              <i className="icon-phone-number footer-support-phone" aria-hidden='true'></i>
-              <span className="sr-only">Report Network Issue: Phone&nbsp;</span>
-              {this.geoStore.networkIssueNumber}
-            </span>
-          </a>
-        </p>
-      </div>
+      <p className="network-contact-info">
+        <span aria-hidden="true">Report Network Issue:</span>
+        <br className="visible-md-inline"/>
+        <a href={'tel:' + this.geoStore.networkIssueNumber}>
+          <span>
+            <i className="icon-phone-number footer-support-phone" aria-hidden='true'></i>
+            <span className="sr-only">Report Network Issue: Phone&nbsp;</span>
+            {this.geoStore.networkIssueNumber}
+          </span>
+        </a>
+      </p>
     )
   }
 
   renderNetworkLegend = () => {
     return (
       <div>
-        <span className="visible-sm">
+        <span className="hidden-xs hidden-sm">
           {this.renderNetworkSubscriptionLink()}
-          {this.renderContactInfo()}
-          <hr className="visible-sm col-xs-12"/>
         </span>
-        <h2 className="as-h5">
+        <h2>
           Network<span className="sr-only">&nbsp;color key</span>
         </h2>
         <div className="key-labels">
@@ -186,7 +202,7 @@ export default class GeolinkControls extends React.Component {
   renderWeatherLegend = () => {
     return (
       <div>
-        <h2 className="as-h5">Weather<span className="sr-only">&nbsp;color key</span></h2>
+        <h2>Weather<span className="sr-only">&nbsp;color key</span></h2>
         <div className="key-labels">
           <span className="as-label">Light Rain</span>
           <span className="as-label">Heavy Rain</span>
@@ -203,7 +219,7 @@ export default class GeolinkControls extends React.Component {
   renderTrafficLegend = () => {
     return (
       <div>
-        <h2 className="as-h5">Traffic<span className="sr-only">&nbsp;color key</span></h2>
+        <h2>Traffic<span className="sr-only">&nbsp;color key</span></h2>
         <div className="traffic-legend-wrapper ">
           <div className="traffic-bars">
             <div className="key-labels traffic">
