b15b3f1fa0924e0aa150fbfdf0c84415d25f0b47
Katy Williamson <kwilliamson@sapient.com>
2017-10-26 12:28:55 -0400

    desktop styles for admin page reflow

--- src/pages/admin-dashboard.page.jsx
+++ src/pages/admin-dashboard.page.jsx
@@ -20,6 +20,8 @@ export default class AdminDashboardPage extends React.Component {
   }
 
   render() {
+    const isPermitted = this.store.destinationIsPermitted;
+    const hideAside = !(isPermitted.shopStandardDevices || isPermitted.shopSpecializedDevices || isPermitted.shopPublicSafetySolutions);
     return (
       <article id="admin-dashboard-page">
         <div className="container">
@@ -27,13 +29,13 @@ export default class AdminDashboardPage extends React.Component {
             <PageTitle>Administration</PageTitle>
           </div>
           <div className="row no-gutters">
-            <section className="col-xs-12 col-lg-8 manage-actions">
-              <div className="col-xs-12">
+            <section className={`col-xs-12 ${hideAside ? 'hide-aside' : 'col-lg-8'} manage-actions`}>
+              {!hideAside && <div className="col-xs-12">
                 <h2 className="as-h4">Management</h2>
-              </div>
+              </div>}
               <nav>
                 <ul>
-                  {this.store.destinationIsPermitted.manageUsers && <li className="col-xs-12">
+                  {isPermitted.manageUsers && <li className="col-xs-12">
                     <NewTabLink to={config.manageUsersLink} className="dashboard-card manage-users has-shadow">
                       <div className="desc">
                         <h3>Manage users</h3>
@@ -42,7 +44,7 @@ export default class AdminDashboardPage extends React.Component {
                       <span>Manage Users <i className="icon-arrowRight" aria-hidden="true"></i></span>
                     </NewTabLink>
                   </li>}
-                  {this.store.destinationIsPermitted.manageApps &&   <li className="col-xs-12 col-sm-6">
+                  {isPermitted.manageApps && <li className="col-xs-12">
                     <Link to="/admin/manage-apps" className="dashboard-card manage-apps has-shadow">
                       <div className="desc">
                         <h3>Manage apps</h3>
@@ -51,7 +53,7 @@ export default class AdminDashboardPage extends React.Component {
                       <span>Manage apps <i className="icon-arrowRight" aria-hidden="true"></i></span>
                     </Link>
                   </li>}
-                  <li className="col-xs-12 col-sm-6">
+                  {isPermitted.manageBilling && <li className="col-xs-12">
                     <NewTabLink to={config.manageServicesLink} className="dashboard-card manage-services has-shadow">
                       <div className="desc">
                         <h3>Manage services & billing</h3>
@@ -59,8 +61,8 @@ export default class AdminDashboardPage extends React.Component {
                       </div>
                       <span>Manage services & billing <i className="icon-arrowRight" aria-hidden="true"></i></span>
                     </NewTabLink>
-                  </li>
-                  {this.store.destinationIsPermitted.manageVoicemail && <li className="col-xs-12 col-sm-6">
+                  </li>}
+                  {isPermitted.manageVoicemail && <li className="col-xs-12">
                     <NewTabLink to={config.manageVoicemailAndUsageLink} className="dashboard-card manage-voicemail-and-usage has-shadow">
                       <div className="desc">
                         <h3>Manage voicemail &amp; usage</h3>
@@ -69,7 +71,7 @@ export default class AdminDashboardPage extends React.Component {
                       <span>Manage voicemail &amp; usage <i className="icon-arrowRight" aria-hidden="true"></i></span>
                     </NewTabLink>
                   </li>}
-                  {this.store.destinationIsPermitted.viewReports && <li className="col-xs-12 col-sm-6">
+                  {isPermitted.viewReports && <li className="col-xs-12">
                     <NewTabLink to={config.viewWirelessReportsLink} className="dashboard-card manage-wireless-reports has-shadow">
                       <div className="desc">
                         <h3>View wireless reports</h3>
@@ -81,13 +83,13 @@ export default class AdminDashboardPage extends React.Component {
                 </ul>
               </nav>
             </section>
-            <aside className="col-xs-12 col-lg-4 shop-actions">
+            {!hideAside && <aside className="col-xs-12 col-lg-4 shop-actions">
               <div className="col-xs-12">
                 <h2 className="as-h4">Purchasing &amp; Provisioning</h2>
               </div>
               <nav>
                 <ul>
-                  {this.store.destinationIsPermitted.shopStandardDevices && <li className="col-xs-12 col-md-6 col-lg-12">
+                  {isPermitted.shopStandardDevices && <li className="col-xs-12 col-md-6 col-lg-12">
                     <NewTabLink to={config.shopStandardDevicesLink} className="dashboard-card shop-devices-rates has-shadow">
                       <div className="desc">
                         <h3>Shop standard devices &amp; rate plans</h3>
@@ -96,7 +98,7 @@ export default class AdminDashboardPage extends React.Component {
                       <span>Shop Devices &amp; Plans <i className="icon-arrowRight" aria-hidden="true"></i></span>
                     </NewTabLink>
                   </li>}
-                  {this.store.destinationIsPermitted.shopSpecializedDevices && <li className="col-xs-12 col-md-6 col-lg-12">
+                  {isPermitted.shopSpecializedDevices && <li className="col-xs-12 col-md-6 col-lg-12">
                     <Link to="/admin/devices" className="dashboard-card shop-specialized-devices has-shadow">
                       <div className="desc">
                         <h3>Shop specialized devices</h3>
@@ -105,7 +107,7 @@ export default class AdminDashboardPage extends React.Component {
                       <span>Shop Specialized Devices<i className="icon-arrowRight" aria-hidden="true"></i></span>
                     </Link>
                   </li>}
-                  {this.store.destinationIsPermitted.shopPublicSafetySolutions && <li className="col-xs-12 col-md-6 col-lg-12">
+                  {isPermitted.shopPublicSafetySolutions && <li className="col-xs-12 col-md-6 col-lg-12">
                     <Link to="/admin/solutions" className="dashboard-card shop-solutions has-shadow">
                       <div className="desc">
                         <h3>Shop public safety solutions</h3>
@@ -116,7 +118,7 @@ export default class AdminDashboardPage extends React.Component {
                   </li>}
                 </ul>
               </nav>
-            </aside>
+            </aside>}
           </div>
         </div>
       </article>
