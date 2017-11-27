e0f0b0282ed865197a395e286595333a9830940b
Justin Isaf <jisaf@sapient.com>
2017-11-27 15:23:30 -0500

    abstracted further based on comments

--- src/pages/admin-dashboard.page.jsx
+++ src/pages/admin-dashboard.page.jsx
@@ -4,6 +4,7 @@ import {Link} from 'react-router-dom';
 import {observer, inject} from 'mobx-react';
 import NewTabLink from '../components/link/new-tab-link';
 import config from 'config';
+import {adminCards, asideCards} from '../content/admin-cards.js';
 import PageTitle from '../components/page-title/page-title';
 
 @inject('store')
@@ -13,30 +14,33 @@ export default class AdminDashboardPage extends React.Component {
   static propTypes = {
     store: PropTypes.object
   }
-
+  
   constructor(props) {
     super(props);
     this.store = this.props.store.userStore;
   }
-
-  permissionedCard(isPermitted, linkTo, className, header, description, callToAction = header) {
-    //check if we should use Link or NewTabLink
-    let LinkType = linkTo[0] === '/' ? Link : NewTabLink;
-    if(isPermitted){
+  
+  renderPermissionedCard(card, key = 1) {
+    //check if we are using a local or extenrnal link, and act accordingly.
+    let LinkType = card.linkTo[0] === '/' ? Link : NewTabLink;
+    let linkDest = card.linkTo[0] === '/' ? card.linkTo : config[card.linkTo]
+    //render if we are allowed
+    const isPermitted = this.store.destinationIsPermitted;
+    if(isPermitted[card.isPermitted]){
       return (
-        <li className="col-xs-12">
-          <LinkType to={linkTo} className={`dashboard-card ${className} has-shadow`}>
+        <li className="col-xs-12" key={key}>
+          <LinkType to={linkDest} className={`dashboard-card ${card.className} has-shadow`}>
             <div className="desc">
-              <h3>{header}</h3>
-              <p>{description}</p>
+              <h3>{card.header}</h3>
+              <p>{card.description}</p>
             </div>
-            <span>{callToAction} <i className="icon-arrowRight" aria-hidden="true"></i></span>
+            <span>{card.callToAction || card.header} <i className="icon-arrowRight" aria-hidden="true"></i></span>
           </LinkType>
         </li>
       )
     }
   }
-
+  
   render() {
     const isPermitted = this.store.destinationIsPermitted;
     const hideAside = !(isPermitted.shopStandardDevices || isPermitted.shopSpecializedDevices || isPermitted.shopPublicSafetySolutions);
@@ -53,48 +57,9 @@ export default class AdminDashboardPage extends React.Component {
               </div>}
               <nav>
                 <ul>
-                  {this.permissionedCard(
-                    isPermitted.manageUsers,
-                    config.manageUsersLink,
-                    'manage-users',
-                    'Manage users',
-                    'Add, edit and remove users'
-                  )}
-                  {this.permissionedCard(
-                    isPermitted.manageApps,
-                    '/admin/manage-apps',
-                    'manage-apps',
-                    'Manage apps',
-                    'Push an app to your Mobile Device Management(MDM) solution, recommend apps, block apps',
-                  )}
-                  {this.permissionedCard(
-                    isPermitted.manageBilling,
-                    config.manageServicesLink,
-                    'manage-services',
-                    'Manage services & billing',
-                    'Assign or remove devices, change rate plans &amp; features, view & pay bills, update information, manage push-to-talk'
-                  )}
-                  {this.permissionedCard(
-                    isPermitted.manageIotDevices,
-                    config.manageIotDevicesLink,
-                    'manage-iot',
-                    'Manage IoT devices',
-                    'Manage the connectivity of your IoT devices'
-                  )}
-                  {this.permissionedCard(
-                    isPermitted.manageVoicemail,
-                    config.manageVoicemailAndUsageLink,
-                    'manage-voicemail-and-usage',
-                    'Manage voicemail &amp; usage',
-                    'Manage voicemail and data usage for your devices'
-                  )}
-                  {this.permissionedCard(
-                    isPermitted.viewReports,
-                    config.viewWirelessReportsLink,
-                    'manage-wireless-reports',
-                    'View wireless reports',
-                    'View device inventory, rate plan summary, early termination fees, upgrade eligibility, device unlock eligibility'
-                  )}
+                  {adminCards.map((card, i) => {
+                    return this.renderPermissionedCard(card, i)
+                  })}
                 </ul>
               </nav>
             </section>
@@ -104,28 +69,9 @@ export default class AdminDashboardPage extends React.Component {
               </div>
               <nav>
                 <ul>
-                  {this.permissionedCard(
-                    isPermitted.shopStandardDevices,
-                    config.shopStandardDevicesLink,
-                    'shop-devices-rates',
-                    'Shop standard devices &amp; rate plans',
-                    'Add a new device, provision an existing device, add a rate plan, feature(s) and accessories',
-                    'Shop Devices &amp; Plans'
-                  )}
-                  {this.permissionedCard(
-                    isPermitted.shopSpecializedDevices,
-                    '/admin/devices',
-                    'shop-specialized-devices',
-                    'Shop specialized devices',
-                    'Purchase ruggedized devices, vehicle routers, etc.'
-                  )}
-                  {this.permissionedCard(
-                    isPermitted.shopPublicSafetySolutions,
-                    '/admin/solutions',
-                    'shop-solutions',
-                    'Shop public safety solutions',
-                    'Browse public safety solutions and choose which are best for your organization',
-                  )}
+                  {asideCards.map((card, i) => {
+                    return this.renderPermissionedCard(card, i)
+                  })}
                 </ul>
               </nav>
             </aside>}
