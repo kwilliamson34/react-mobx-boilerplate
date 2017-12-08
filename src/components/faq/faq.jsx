bc5d250bd069446a7123757c3995820a05e42ac8
Nassir Isaf <nisaf@sapient.com>
2017-12-08 12:04:33 -0500

    tweaks based on PRs, cleared out permissions arrays. with no permissions, all items show.

--- src/components/faq/faq.jsx
+++ src/components/faq/faq.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {observer} from 'mobx-react';
-import {computed, observable} from 'mobx';
+import {observable} from 'mobx';
 import _ from 'lodash';
 
 import PageTitle from '../page-title/page-title';
@@ -18,6 +18,11 @@ export class Faq extends React.Component {
   constructor(props) {
     super(props);
     this.faq = this.props.faqData;
+    this.allAvailablePermissions = [];
+  }
+
+  componentWillMount() {
+    this.allAvailablePermissions = this.findAvailablePermissions();
   }
 
   componentWillUnmount() {
@@ -26,7 +31,7 @@ export class Faq extends React.Component {
 
   @observable activeCategory = 'All';
 
-  @computed get allAvailablePermissions() {
+  findAvailablePermissions = () => {
     let allPermissions = [];
     for (let category in this.faq.categories) {
       this.faq.categories[category].permissions.forEach(permission => {
@@ -38,15 +43,15 @@ export class Faq extends React.Component {
     return allPermissions;
   }
 
-  @computed get permissionedCategories() {
+  permissionedCategories = () => {
     return _.intersection(this.allAvailablePermissions, this.props.userPermissions).length > 0
       ? this.faq.categories.filter(category => _.intersection(category.permissions, this.props.userPermissions).length > 0)
       : this.faq.categories;
   }
 
-  @computed get filteredFaqEntries() {
+  filteredFaqEntries = () => {
     if (this.activeCategory === 'All') {
-      return this.faq.entries.filter(faq => this.permissionedCategories.map(category => category.title).indexOf(faq.category) > -1);
+      return this.faq.entries.filter(faq => this.permissionedCategories().map(category => category.title).indexOf(faq.category) > -1);
     } else {
       return this.faq.entries.filter(faq => faq.category === this.activeCategory);
     }
@@ -88,7 +93,7 @@ export class Faq extends React.Component {
     return (
       <div className="faq-category-select">
         <form>
-          <label htmlFor="faqCategory">FILTER BY CATEGORY</label>
+          <label htmlFor="faqCategory">Filter By Category</label>
           <select id="faqCategory" className="form-control" onChange={this.updateCategory} value={this.activeCategory}>
             <option value="All">All Categories</option>
             {categories.map((category, i) => {
@@ -105,7 +110,7 @@ export class Faq extends React.Component {
   renderFaqEntriesList = () => {
     return (
       <div className="faq-entry-list">
-        {this.filteredFaqEntries.map((entry, i) => {
+        {this.filteredFaqEntries().map((entry, i) => {
           return (
             <article key={i} className="faq-entry">
               <h3 id={'faq-' + i}>{entry.question}</h3>
@@ -137,10 +142,10 @@ export class Faq extends React.Component {
           <div className="row">
             <div className="faq-header col-xs-12 col-sm-10 col-sm-offset-1">
               <div className="hidden-xs">
-                {this.renderCategoriesAsButtons(this.permissionedCategories)}
+                {this.renderCategoriesAsButtons(this.permissionedCategories())}
               </div>
               <div className="hidden-sm hidden-md hidden-lg">
-                {this.renderCategoriesAsSelectMenu(this.permissionedCategories)}
+                {this.renderCategoriesAsSelectMenu(this.permissionedCategories())}
               </div>
               <div className="horizontal-line-header">
                 <h2>
