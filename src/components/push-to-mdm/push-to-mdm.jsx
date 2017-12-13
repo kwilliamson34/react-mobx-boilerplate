a5f5a77fd148f1440048c2114253eb488e5faa42
Katy Williamson <kwilliamson@sapient.com>
2017-12-13 11:56:05 -0500

    FPSE-1426 adjust mdm button to handle DISABLED case; fix broken aria live text using observable

--- src/components/push-to-mdm/push-to-mdm.jsx
+++ src/components/push-to-mdm/push-to-mdm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import {observer} from 'mobx-react';
+import {observable, computed} from 'mobx';
 
 @observer
 export class PushToMDM extends React.Component {
@@ -14,33 +14,49 @@ export class PushToMDM extends React.Component {
     appCatalogMDMStatuses: PropTypes.object
   }
 
-  constructor(props) {
-    super(props);
-    this.ariaLiveMessage = '';
+  static defaultProps = {
+    configuredMDMType: '',
+    appCatalogMDMStatuses: {}
   }
 
-  handleButtonClick = (event) => {
-    event.preventDefault();
-    if(this.props.configuredMDMType){
+  @computed get currentStatus() {
+    return this.props.appCatalogMDMStatuses[this.props.psk];
+  }
+  @computed get isConfigured() {
+    return this.props.configuredMDMType !== '';
+  }
+  @computed get isDisabled() {
+    return !this.isConfigured || this.currentStatus === 'DISABLED';
+  }
+  @observable ariaLiveMessage = '';
+
+  handleClick = () => {
+    const configured = this.props.configuredMDMType !== '';
+    if(!configured){
+      this.ariaLiveMessage = 'Push to MDM is not available. Configure an MDM to push apps to the system.';
+    } else if (this.currentStatus === 'DISABLED') {
+      this.ariaLiveMessage = 'Push to MDM is not available for this app.';
+    } else {
       this.ariaLiveMessage = 'App submission in progress.';
       this.props.pushToMDM(this.props.psk).then(() => {
         /* Must push a non-empty string for live region to recognize a change.
         This string is not actually announced; it's pre-empted by the alert bar. */
         this.ariaLiveMessage = 'Done.';
       });
-    } else {
-      this.ariaLiveMessage = 'Push to MDM is not available. Configure an MDM to push apps to the system.';
     }
   }
 
+  handleBlur = () => {
+    this.ariaLiveMessage = '';
+  }
+
   render() {
-    let enabled = this.props.configuredMDMType !== '';
     let btnLabel = 'Push to MDM';
     let btnClass = 'fn-primary';
     let iconClass = '';
 
-    if(enabled) {
-      switch(this.props.appCatalogMDMStatuses[this.props.psk]) {
+    if(this.isConfigured) {
+      switch(this.currentStatus) {
         case 'IN_PROGRESS':
         case 'PENDING':
           btnClass = 'fn-primary deaden';
@@ -65,8 +81,17 @@ export class PushToMDM extends React.Component {
 
     return (
       <div>
-        <span className="sr-only" role="alert" aria-live="assertive">{this.ariaLiveMessage}</span>
-        <button type="button" id={'pushBtn' + this.props.psk} onClick={this.handleButtonClick} className={`push-button ${btnClass} ${enabled ? '' : 'disabled'}`}>
+        <span className="sr-only" role="alert" aria-live="assertive">
+          {this.ariaLiveMessage}
+        </span>
+        <button
+          type="button"
+          id={'pushBtn' + this.props.psk}
+          onClick={this.handleClick}
+          onBlur={this.handleBlur}
+          aria-disabled={this.isDisabled}
+          className={`push-button ${btnClass}`}>
+
           {iconClass && <i className={iconClass} aria-hidden="true"></i>}
           <span dangerouslySetInnerHTML={{__html: btnLabel}}></span>
         </button>
