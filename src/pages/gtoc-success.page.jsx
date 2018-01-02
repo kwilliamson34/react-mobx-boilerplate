e004804001215409d7aa10834c3f7295a23d7c42
Paul Arce <parce@sapient.com>
2018-01-02 09:02:27 -0500

    FPSE-1843: update 14 days to 24 hours, reflecting prod change from IOC1

--- src/pages/gtoc-success.page.jsx
+++ src/pages/gtoc-success.page.jsx
@@ -12,7 +12,7 @@ export default class SubscribeToGTOCSuccess extends React.Component {
           <div className="col-xs-12 col-md-offset-1 col-md-10 col-lg-offset-1 col-lg-10">
             <PageTitle>We&apos;ve received your request.</PageTitle>
             <p>
-              Processing your subscription can take up to 14 days, after which you will start receiving alert notifications. If you have any questions, please contact<br className="visible-lg-block"/> FirstNet Customer Service at&nbsp;
+              Processing your subscription can take up to 24 hours, after which you will start receiving alert notifications. If you have any questions, please contact<br className="visible-lg-block"/> FirstNet Customer Service at&nbsp;
               <a href={'tel:' + config.attCustomerSupportPhone}>
 								<span className="sr-only">FirstNet Customer Service Phone&nbsp;</span>
 								{config.attCustomerSupportPhone}
