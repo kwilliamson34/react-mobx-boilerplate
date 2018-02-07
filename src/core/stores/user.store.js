94f82b807b6803ec3105f2f2b51570950d433d34
Justin Isaf <jisaf@sapient.com>
2018-02-07 12:39:59 -0500

    only group with access to wireless reports is ADM

--- src/core/stores/user.store.js
+++ src/core/stores/user.store.js
@@ -146,7 +146,7 @@ class UserStore {
     shopPublicSafetySolutions: ['G_FN_ADM', 'G_FN_VOL'],
     manageUsers: ['G_FN_ADM', 'G_FN_ITM', 'G_FN_VOL_ADM'],
     manageBilling: ['G_FN_ADM', 'G_FN_VOL'],
-    viewReports: ['G_FN_ADM', 'G_FN_VOL'],
+    viewReports: ['G_FN_ADM'],
     manageApps: ['G_FN_ADM', 'G_FN_ITM', 'G_FN_VOL_ADM'],
     manageVoicemail: ['G_FN_SUB'],
     administration: ['G_FN_ADM', 'G_FN_ITM', 'G_FN_SUB', 'G_FN_VOL_ADM', 'G_FN_VOL'],
