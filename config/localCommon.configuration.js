837341a040a6107b22b09b9b849907e57a34a959
Katy Williamson <kwilliamson@sapient.com>
2018-03-26 14:09:06 -0400

    remove env-specific duplicates, oops

--- config/localCommon.configuration.js
+++ config/localCommon.configuration.js
@@ -2,16 +2,7 @@ var localCommonConfig = {
   httpProtocol: 'https://',
   ssoCrack: 'https://localhost:8443/oauth/validate',
   apperianUploads: 'https://ease.apperian.com/uploads/',
-  cloudGeolinkAssets: 'https://onemap.att.com/icecapmap',
-
-  manageMyProfileLink: 'https://test-profilemgt.firstnet.att.com/ebiz/firstnet/userDetails/viewMyProfile',
-  manageUsersLink: 'https://test-profilemgt.firstnet.att.com/ebiz/firstnet/index.jsp#/companyInfo/companyInfoProfile',
-  manageServicesLink: 'https://test-wireless.firstnet.att.com/b2bservlets/HaloSSOLoginServlet.dyn',
-  manageVoicemailAndUsageLink: 'https://test-wireless.firstnet.att.com/b2bservlets/HaloSSOLoginServlet.dyn',
-  manageIotDevicesLink: 'https://fnstaging.jasper.com/provision/jsp/login.jsp',
-  incidentUpliftLink: 'https://test-incidentmgt.firstnet.att.com/',
-  viewWirelessReportsLink: 'https://test-wireless.firstnet.att.com/b2bservlets/HaloSSOLoginServlet.dyn?requestedPage=downloadReports',
-  shopStandardDevicesLink: 'https://test-wireless.firstnet.att.com/b2bservlets/HaloSSOLoginServlet.dyn'
+  cloudGeolinkAssets: 'https://onemap.att.com/icecapmap'
 }
 
 export default localCommonConfig
